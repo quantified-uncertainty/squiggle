b65eb0b01f8c6492c7a0a916bebd5a815835ff68
Sam Nolan <samnolan555@gmail.com>
2022-03-18 14:07:43 +1100

    Move to Webpack

---
+++ packages/squiggle-lang/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'squiggle_lang',
+      type: 'umd',
+    },
+  },
+};
