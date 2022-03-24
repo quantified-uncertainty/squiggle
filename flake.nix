9b14a5c8cf0b5a0fa87236e18fbb29b5ff15649b
Quinn Dougherty <quinnd@tutanota.com>
2022-03-24 12:33:37 -0400

    removed flake-compat

--- flake.nix
+++ flake.nix
@@ -2,23 +2,18 @@
   description = "Building codium for rescript development";
 
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     { self
     , nixpkgs
-    , flake-compat
     }:
     let
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 self.lastModifiedDate;
       # System types to support.
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       # Nixpkgs instantiated for supported system types.
