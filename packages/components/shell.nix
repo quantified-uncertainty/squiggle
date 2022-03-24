9a751c82cf6fdff3f7d311151b2d71658029a9a4
Quinn Dougherty <quinnd@tutanota.com>
2022-03-24 17:29:21 -0400

    removed shell.nix files

--- packages/components/shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell { 
-  name = "squiggle-components";
-  buildInputs = with pkgs; [ nodePackages.yarn nodejs ];
-}
--- shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  name = "squiggle-root";
-  buildInputs = with pkgs; [ nodePackages.yarn ];
-}
