f4085e93ad52a34cda43d50c0b6499c16bd1246a
Sam Nolan <samnolan555@gmail.com>
2022-05-02 18:29:59 +0000

    Real log scales

--- packages/components/src/components/DistributionChart.tsx
+++ packages/components/src/components/DistributionChart.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import _ from "lodash";
-import type { Spec } from "vega";
 import type { Distribution } from "@quri/squiggle-lang";
-import { distributionErrorToString } from "@quri/squiggle-lang";
-import { createClassFromSpec } from "react-vega";
+import { distributionErrorToString, result, shape } from "@quri/squiggle-lang";
+import { Vega, VisualizationSpec } from "react-vega";
 import * as chartSpecification from "../vega-specs/spec-distributions.json";
 import { ErrorBox } from "./ErrorBox";
-import styled from "styled-components";
-
-let SquiggleVegaChart = createClassFromSpec({
-  spec: chartSpecification as Spec,
-});
+import {
+  linearXScale,
+  logXScale,
+  linearYScale,
+  expYScale,
+} from "./DistributionVegaScales";
 
 type DistributionChartProps = {
   distribution: Distribution;
@@ -23,17 +23,30 @@ export const DistributionChart: React.FC<DistributionChartProps> = ({
   width,
   height,
 }: DistributionChartProps) => {
+  let [isLogX, setLogX] = React.useState(false);
+  let [isExpY, setExpY] = React.useState(false);
   let shape = distribution.pointSet();
   if (shape.tag === "Ok") {
-    let widthProp = width ? width - 20 : undefined;
-    var result = (
-      <SquiggleVegaChart
-        data={{ con: shape.value.continuous, dis: shape.value.discrete }}
-        width={widthProp}
-        height={height}
-        actions={false}
-      />
-    );
+    let spec = buildSpec(isLogX, isExpY, shape.value);
+    if (spec.tag == "Ok") {
+      let widthProp = width ? width - 20 : undefined;
+      var result = (
+        <div>
+          <Vega
+            spec={spec.value}
+            data={{ con: shape.value.continuous, dis: shape.value.discrete }}
+            width={widthProp}
+            height={height}
+            actions={false}
+          />
+        </div>
+      );
+    } else {
+      // Log scales don't work when you have points below 0
+      var result = (
+        <ErrorBox heading={spec.value.heading}>{spec.value.error}</ErrorBox>
+      );
+    }
   } else {
     var result = (
       <ErrorBox heading="Distribution Error">
@@ -41,5 +54,55 @@ export const DistributionChart: React.FC<DistributionChartProps> = ({
       </ErrorBox>
     );
   }
-  return result;
+  return (
+    <>
+      {result}
+      <div>
+        <CheckBox label="Log X scale" value={isLogX} onChange={setLogX} />
+        <CheckBox label="Exp Y scale" value={isExpY} onChange={setExpY} />
+      </div>
+    </>
+  );
+};
+
+type ViewError = { heading: string; error: string };
+
+function buildSpec(
+  isLogX: boolean,
+  isExpY: boolean,
+  shape: shape
+): result<VisualizationSpec, ViewError> {
+  let someBelow0 =
+    shape.continuous.some((x) => x.x <= 0) ||
+    shape.discrete.some((x) => x.x <= 0);
+  if (!(isLogX && someBelow0)) {
+    return {
+      tag: "Ok",
+      value: {
+        ...chartSpecification,
+        scales: [
+          isLogX ? logXScale : linearXScale,
+          isExpY ? expYScale : linearYScale,
+        ],
+      } as VisualizationSpec,
+    };
+  } else {
+    return {
+      tag: "Error",
+      value: {
+        heading: "Log Viewing error",
+        error:
+          "Distribution contains values lower than or equal to 0. Cannot view",
+      },
+    };
+  }
+}
+
+export const CheckBox = ({ label, onChange, value }) => {
+  return (
+    <span>
+      <input type="checkbox" value={value} onChange={() => onChange(!value)} />
+      <label>{label}</label>
+    </span>
+  );
 };
