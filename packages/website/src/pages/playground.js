35a6a0c4e5518bd643c7ab98e1f23afa8d2335ee
Recursing <buonanno.lorenzo@gmail.com>
2022-06-21 23:09:42 +0200

    Save playground state in URL, for easy sharing
    fix ssr

--- packages/website/src/pages/playground.js
+++ packages/website/src/pages/playground.js
@@ -2,7 +2,36 @@ import React from "react";
 import Layout from "@theme/Layout";
 import { SquigglePlayground } from "../components/SquigglePlayground";
 
+function getHashData() {
+  if (typeof window === "undefined") {
+    return {};
+  }
+  try {
+    return JSON.parse(window.decodeURIComponent(window.location.hash.slice(1)));
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+}
+
+function setHashData(Data) {
+  window.location.hash = window.encodeURIComponent(
+    JSON.stringify({ ...getHashData(), ...Data })
+  );
+}
+
 export default function PlaygroundPage() {
+  const playgroundProps = {
+    initialSquiggleString: "normal(0,1)",
+    height: 700,
+    showTypes: true,
+    ...getHashData(),
+    onCodeChange: (code) => setHashData({ initialSquiggleString: code }),
+    onSettingsChange: (settings) => {
+      const { showTypes, showControls, showSummary, showEditor } = settings;
+      setHashData({ showTypes, showControls, showSummary, showEditor });
+    },
+  };
   return (
     <Layout title="Playground" description="Squiggle Playground">
       <div
@@ -10,11 +39,7 @@ export default function PlaygroundPage() {
           maxWidth: 2000,
         }}
       >
-        <SquigglePlayground
-          initialSquiggleString="normal(0,1)"
-          height={700}
-          showTypes={true}
-        />
+        <SquigglePlayground {...playgroundProps} />
       </div>
     </Layout>
   );
