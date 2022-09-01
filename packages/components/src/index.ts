4a3c982c575e61916d7c7ba79810cc57f6a53f83
Vyacheslav Matyukhin <me@berekuk.ru>
2022-09-01 14:40:56 +0400

    remove SquiggleEditorWithImportedBindings

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,4 +2,3 @@ export { SquiggleChart } from "./components/SquiggleChart";
 export { SquiggleEditor } from "./components/SquiggleEditor";
 export { SquigglePlayground } from "./components/SquigglePlayground";
 export { SquiggleContainer } from "./components/SquiggleContainer";
-export { SquiggleEditorWithImportedBindings } from "./components/SquiggleEditorWithImportedBindings";
