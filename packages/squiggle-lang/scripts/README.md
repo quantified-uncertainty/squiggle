6463b52052e712af2e035bd980f8a52b6f7dfc3f
Vyacheslav Matyukhin <me@berekuk.ru>
2022-09-15 15:17:19 +0400

    benchmark and other scripts

---
+++ packages/squiggle-lang/scripts/README.md
@@ -0,0 +1,3 @@
+Various scripts used for development, benchmarking and testing.
+
+None of these are bundled in NPM package yet.
--- packages/squiggle-lang/scripts/bench-map-reduce.js
+++ packages/squiggle-lang/scripts/bench-map-reduce.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const s = require("./dist/src/js");
+const s = require("@quri/squiggle-lang");
 
 const measure = (cb, times = 1) => {
   const t1 = new Date();
@@ -11,17 +11,17 @@ const measure = (cb, times = 1) => {
   return (t2 - t1) / 1000;
 };
 
-const maxP = 7;
+const maxP = 5;
 
 for (let p = 0; p <= maxP; p++) {
   const size = Math.pow(10, p);
   const prj = s.SqProject.create();
-  prj.setSource("list", `l = List.upTo(1,${size})`);
-  prj.run("list");
-  prj.setSource("map", "l |> map({|x| x})");
-  prj.setContinues("map", ["list"]);
+  prj.setSource(
+    "main",
+    `List.upTo(1, ${size}) |> map({|x| List.upTo(1, 100) |> reduce(0, {|a,b|a+b})})`
+  );
   const t = measure(() => {
-    prj.run("map");
+    prj.run("main");
   });
   console.log(`1e${p}`, "\t", t);
 }
