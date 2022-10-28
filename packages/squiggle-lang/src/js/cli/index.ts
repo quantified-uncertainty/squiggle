c0f5eea99bd95fa9e2b6f5f6c6fee5180f1fb999
Vyacheslav Matyukhin <me@berekuk.ru>
2022-10-28 19:47:45 +0400

    includes in CLI; basic tests

--- packages/squiggle-lang/src/js/cli/index.ts
+++ packages/squiggle-lang/src/js/cli/index.ts
@@ -1,61 +1,69 @@
 import fs from "fs";
 import { OutputMode, run } from "./utils";
 
-import { program } from "@commander-js/extra-typings";
+import { Command } from "@commander-js/extra-typings";
 import open from "open";
 
-program
-  .command("run")
-  .arguments("[filename]")
-  .option(
-    "-e, --eval <code>",
-    "run a given squiggle code string instead of a file"
-  )
-  .option("-t --time", "output the time it took to evaluate the code")
-  .option("-q, --quiet", "don't output the results and bindings") // useful for measuring the performance or checking that the code is valid
-  .option("-b, --show-bindings", "show bindings even if the result is present") // incompatible with --quiet
-  .action((filename, options) => {
-    let src = "";
+export const makeProgram = () => {
+  const program = new Command();
 
-    let output: OutputMode = "RESULT_OR_BINDINGS";
-    if (options.quiet && options.showBindings) {
-      program.error(
-        "--quiet and --show-bindings can't be set at the same time."
-      );
-    } else if (options.quiet) {
-      output = "NONE";
-    } else if (options.showBindings) {
-      output = "RESULT_AND_BINDINGS";
-    }
+  program
+    .command("run")
+    .arguments("[filename]")
+    .option(
+      "-e, --eval <code>",
+      "run a given squiggle code string instead of a file"
+    )
+    .option("-t --time", "output the time it took to evaluate the code")
+    .option("-q, --quiet", "don't output the results and bindings") // useful for measuring the performance or checking that the code is valid
+    .option(
+      "-b, --show-bindings",
+      "show bindings even if the result is present"
+    ) // incompatible with --quiet
+    .action((filename, options) => {
+      let src = "";
 
-    if (filename) {
-      if (options.eval) {
-        program.error("Only one of filename and eval string should be set.");
+      let output: OutputMode = "RESULT_OR_BINDINGS";
+      if (options.quiet && options.showBindings) {
+        program.error(
+          "--quiet and --show-bindings can't be set at the same time."
+        );
+      } else if (options.quiet) {
+        output = "NONE";
+      } else if (options.showBindings) {
+        output = "RESULT_AND_BINDINGS";
       }
-      src = fs.readFileSync(filename, "utf-8");
-    } else {
-      if (options.eval) {
-        src = options.eval;
+
+      if (filename) {
+        if (options.eval) {
+          program.error("Only one of filename and eval string should be set.");
+        }
+        src = fs.readFileSync(filename, "utf-8");
       } else {
-        program.error("One of filename and eval string should be set.");
+        if (options.eval) {
+          src = options.eval;
+        } else {
+          program.error("One of filename and eval string should be set.");
+        }
       }
-    }
 
-    const sampleCount = process.env.SAMPLE_COUNT;
+      const sampleCount = process.env.SAMPLE_COUNT;
 
-    run(src, {
-      output,
-      measure: options.time,
-      sampleCount,
+      run({ src, output, measure: options.time, sampleCount });
     });
+
+  program.command("playground").action(() => {
+    open("https://www.squiggle-language.com/playground");
   });
 
-program.command("playground").action(() => {
-  open("https://www.squiggle-language.com/playground");
-});
-
-const main = async () => {
-  await program.parseAsync();
+  return program;
 };
 
-main();
+const main = async () => {
+  await makeProgram().parseAsync();
+};
+
+if (require.main === module) {
+  // running as script, https://stackoverflow.com/a/6398335
+  main();
+}
--- packages/squiggle-lang/src/js/cli/utils.ts
+++ packages/squiggle-lang/src/js/cli/utils.ts
@@ -1,5 +1,10 @@
+import fs from "fs";
+import { environment } from "..";
 import { SqProject } from "../SqProject";
 import { SqValueTag } from "../SqValue";
+
+export const red = (str: string) => `\x1b[31;1m${str}\x1b[0m`;
+export const bold = (str: string) => `\x1b[1m${str}\x1b[0m`;
 
 export const measure = (cb: () => void, times = 1) => {
   const t1 = new Date();
@@ -11,61 +16,89 @@ export const measure = (cb: () => void, times = 1) => {
   return (t2.getTime() - t1.getTime()) / 1000;
 };
 
-export const red = (str: string) => `\x1b[31m${str}\x1b[0m`;
-export const green = (str: string) => `\x1b[32m${str}\x1b[0m`;
-
 export type OutputMode = "NONE" | "RESULT_OR_BINDINGS" | "RESULT_AND_BINDINGS";
 
-export type RunProps = {
+export type RunArgs = {
+  src: string;
+  filename?: string;
   output: OutputMode;
   measure?: boolean;
   sampleCount?: string | number;
 };
 
-export const run = (
-  src: string,
-  props: RunProps = { output: "RESULT_OR_BINDINGS" }
-) => {
+const _run = (args: {
+  src: string;
+  filename?: string;
+  environment?: environment;
+}) => {
   const project = SqProject.create();
-  if (props.sampleCount && Number(props.sampleCount) !== NaN) {
-    project.setEnvironment({
-      sampleCount: Number(props.sampleCount),
-      xyPointLength: Number(props.sampleCount),
-    });
-  }
-  project.setSource("main", src);
-  const time = measure(() => project.run("main"));
+  const filename = args.filename || "./__anonymous__";
 
-  const bindings = project.getBindings("main");
-  const result = project.getResult("main");
+  const loadIncludesRecursively = (sourceId: string) => {
+    project.parseIncludes(sourceId);
+    const includes = project.getIncludes(sourceId);
+    if (includes.tag === "Error") {
+      throw new Error(`Failed to parse includes from ${sourceId}`);
+    }
+    includes.value.forEach((includeId) => {
+      const includeSrc = fs.readFileSync(includeId, "utf-8");
+      const source = project.setSource(includeId, includeSrc);
+      loadIncludesRecursively(includeId);
+    });
+  };
+
+  project.setSource(filename, args.src);
+  loadIncludesRecursively(filename);
+
+  const time = measure(() => project.run(filename));
+  const bindings = project.getBindings(filename);
+  const result = project.getResult(filename);
+
+  return { result, bindings, time };
+};
+
+export const run = (args: RunArgs) => {
+  let environment: environment | undefined;
+  if (args.sampleCount && Number(args.sampleCount) !== NaN) {
+    environment = {
+      sampleCount: Number(args.sampleCount),
+      xyPointLength: Number(args.sampleCount),
+    };
+  }
+
+  const { result, bindings, time } = _run({
+    src: args.src,
+    filename: args.filename,
+    environment,
+  });
 
   if (result.tag === "Error") {
     console.log(red("Error:"));
     console.log(result.value.toStringWithStackTrace());
   } else {
-    switch (props.output) {
+    switch (args.output) {
       case "RESULT_OR_BINDINGS":
         if (result.value.tag === SqValueTag.Void) {
           console.log(bindings.toString());
         } else {
           console.log(result.value.toString());
         }
-        if (props.measure) console.log();
+        if (args.measure) console.log();
         break;
       case "RESULT_AND_BINDINGS":
-        console.log(green("Result:"));
+        console.log(bold("Result:"));
         console.log(result.value.toString());
         console.log();
-        console.log(green("Bindings:"));
+        console.log(bold("Bindings:"));
         console.log(bindings.toString());
-        if (props.measure) console.log();
+        if (args.measure) console.log();
         break;
       case "NONE":
       // do nothing
     }
   }
 
-  if (props.measure) {
-    console.log(green("Time:"), String(time) + "s");
+  if (args.measure) {
+    console.log(bold("Time:"), String(time) + "s");
   }
 };
