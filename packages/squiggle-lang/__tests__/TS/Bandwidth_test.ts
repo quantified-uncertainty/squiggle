74ccd85b69495c0c06587d1b3a011c9a6d52cd00
Marshall Lochbaum <mwlochbaum@gmail.com>
2022-11-04 16:06:32 -0400

    Reimplement jstat.percentile and jstat.variance for sorted argument

--- packages/squiggle-lang/__tests__/TS/Bandwidth_test.ts
+++ packages/squiggle-lang/__tests__/TS/Bandwidth_test.ts
@@ -5,11 +5,11 @@ const {
 
 describe("Bandwidth", () => {
   test("nrd0()", () => {
-    const data = [1, 4, 3, 2];
+    const data = [1, 2, 3, 4];
     expect(nrd0(data)).toEqual(0.7625801874014622);
   });
   test("nrd()", () => {
-    const data = [1, 4, 3, 2];
+    const data = [1, 2, 3, 4];
     expect(nrd(data)).toEqual(0.8981499984950554);
   });
 });
