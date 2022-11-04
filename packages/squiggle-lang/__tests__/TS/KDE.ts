00f486eaf489985905df3ce08e9db65528c36142
Marshall Lochbaum <mwlochbaum@gmail.com>
2022-11-04 11:15:25 -0400

    Further explanation for KDE code and test

--- packages/squiggle-lang/__tests__/TS/KDE.ts
+++ packages/squiggle-lang/__tests__/TS/KDE.ts
@@ -4,6 +4,8 @@ import range from "lodash/range";
 import sumBy from "lodash/sumBy";
 
 // KDE by definition, with triangular kernel max(0, |1-x|)
+// https://en.wikipedia.org/wiki/Kernel_density_estimation#Definition
+// Evaluate at the point x
 const evalKde = (samples: number[], width: number, weight: number, x: number) =>
   (weight / width) *
   sumBy(samples, (p) => Math.max(0, 1 - Math.abs((x - p) / width)));
@@ -13,14 +15,18 @@ describe("Kernel density estimation", () => {
     fc.assert(
       fc.property(
         fc.array(fc.double({ min: -10, max: 10, noNaN: true }), {
-          minLength: 6,
+          minLength: 6, // 5 samples or below filtered out in SampleSetDist.toPointSetDist
         }),
-        fc.integer({ min: 4, max: 1e3 }),
+        fc.integer({ min: 4, max: 1e3 }), // 4 is the minimum allowed length
         fc.double({ min: 0.1, max: 3, noNaN: true }),
-        (samples, size, wantedWidth) => {
+        (samples, outputLength, wantedWidth) => {
           const len = samples.length;
           const weight = 1 / len;
           const sortedSamples = samples.sort((a, b) => a - b);
+          // The bandwidth recommendations always return a width less than the
+          // range, so it doesn't make sense to test very large widths.
+          // But fast-check might make the range of the samples arbitrarily
+          // small, so we test here.
           if (sortedSamples[len - 1] - sortedSamples[0] < 2 * wantedWidth)
             return true;
 
@@ -28,26 +34,38 @@ describe("Kernel density estimation", () => {
             xs,
             ys,
             usedWidth: width,
-          } = samplesToContinuousPdf(sortedSamples, size, wantedWidth, weight);
+          } = samplesToContinuousPdf(
+            sortedSamples,
+            outputLength,
+            wantedWidth,
+            weight
+          );
 
-          expect(xs.length).toEqual(size);
-          expect(ys.length).toEqual(size);
+          expect(xs.length).toEqual(outputLength);
+          expect(ys.length).toEqual(outputLength);
 
           // Point range should be sample range plus width on either side
           expect(xs[0] + width).toBeCloseTo(sortedSamples[0]);
-          expect(xs[size - 1] - width).toBeCloseTo(sortedSamples[len - 1]);
+          expect(xs[outputLength - 1] - width).toBeCloseTo(
+            sortedSamples[len - 1]
+          );
 
           // No tail
           expect(ys[0]).toBeCloseTo(0);
-          expect(ys[size - 1]).toBeCloseTo(0);
+          expect(ys[outputLength - 1]).toBeCloseTo(0);
 
-          // Checking cost is proportional to len
+          // Checking cost for each point is proportional to len.
+          // We don't want to spend len * outputLength to check one function:
+          // instead len * numChecks <= 10 * len + 1e3
           const numChecks = 10 + Math.floor(1e3 / len);
-          const checkInds =
-            numChecks >= size
-              ? range(size)
-              : range(numChecks).map(() => Math.floor(size * Math.random()));
-          checkInds.forEach((i) =>
+          const randomOutputIndex = () =>
+            Math.floor(outputLength * Math.random());
+          const allOutputIndices = () => range(outputLength);
+          const indicesToCheck =
+            numChecks >= outputLength
+              ? allOutputIndices()
+              : range(numChecks).map(randomOutputIndex);
+          indicesToCheck.forEach((i) =>
             expect(ys[i]).toBeCloseTo(
               evalKde(sortedSamples, width, weight, xs[i])
             )
--- packages/squiggle-lang/src/rescript/Distributions/SampleSetDist/KdeLibrary.js
+++ packages/squiggle-lang/src/rescript/Distributions/SampleSetDist/KdeLibrary.js
@@ -3,11 +3,11 @@ const isFinite = require("lodash/isFinite");
 
 // Convert samples to x-y pairs for a PDF
 // Uses kernel density estimation (KDE) with a triangular kernel
-// samples: Must be sorted!
-// size:    Number of points in output, >= 4
-// xWidth:  Width of the kernel in x axis units
-// weight:  Probability mass for each point
-const samplesToContinuousPdf = (samples, size, xWidth, weight) => {
+// samples:      Must be sorted!
+// outputLength: Number of points in output, >= 4
+// xWidth:       Width of the kernel in x axis units
+// weight:       Probability mass for each point
+const samplesToContinuousPdf = (samples, outputLength, xWidth, weight) => {
   samples = filter(samples, isFinite); // Not sure if this is needed?
   const len = samples.length;
   if (len === 0) return { xs: [], ys: [] };
@@ -18,59 +18,66 @@ const samplesToContinuousPdf = (samples, size, xWidth, weight) => {
 
   // width is in units of the step size dx and has to be a whole number
   // If we set width = wantedWidth exactly then we have:
-  // width * dx === width * srange / (size - 1 - 2 * width)
-  //            === srange / ((size - 1) / width - 2)
+  // width * dx === width * srange / (outputLength - 1 - 2 * width)
+  //            === srange / ((outputLength - 1) / width - 2)
   //            === srange / ((srange + 2 * xWidth) / xWidth - 2)
   //            === srange / (srange / xWidth + 2 - 2)
   //            === xWidth
-  const wantedWidth = ((size - 1) * xWidth) / (srange + 2 * xWidth);
+  const wantedWidth = ((outputLength - 1) * xWidth) / (srange + 2 * xWidth);
   const width = Math.max(1, Math.floor(wantedWidth));
 
   // To give room for all the kernels, we'll have width steps
   // on either side as padding.
-  // Subtract from the size - 1 total intervals.
-  const stepsInside = size - 1 - 2 * width;
+  // Subtract from the outputLength - 1 total intervals.
+  const stepsInside = outputLength - 1 - 2 * width;
   const dx = srange / stepsInside;
+
+  // Width in x units, not steps
   xWidth = width * dx;
 
   // Output min and range
   // Total number of intervals is:
   // range / dx === (srange + 2 * margin) / dx
   //            === stepsInside + 2 * width
-  //            === size - 1
+  //            === outputLength - 1
   const min = smin - xWidth;
   const range = srange + 2 * xWidth;
 
   // On our discrete set of x values, each triangle is indistinguishable
   // from a sum of two triangles with peaks at the sample's two nearest
   // neighbors. Here we sum up the peak values from all samples.
-  // Only the indices width + 1 + [0, size) in ysum are ever nonzero
+  // Only the indices width + 1 + [0, outputLength) in ysum are ever nonzero
   // The extra zeros are to avoid out-of-bounds lookups for ddy below
-  const ysum = Array(size + 2 * width).fill(0);
+  const ysum = Array(outputLength + 2 * width).fill(0);
   const dxInv = 1 / dx;
   samples.forEach((x) => {
     const off = x - min;
     const index = Math.floor(off * dxInv);
-    const left = off - index * dx;
-    const right = dx - left;
-    ysum[width + index + 1] += right;
-    ysum[width + index + 2] += left;
+    const leftWeight = off - index * dx;
+    const rightWeight = dx - leftWeight;
+    // Offset by 1; see below
+    ysum[width + index + 1] += rightWeight;
+    ysum[width + index + 2] += leftWeight;
   });
 
   // Sum for each sample will be width * width * dx
   // but we want weight / dx
   const normalizer = weight / (xWidth * xWidth);
 
+  const xs = Array(outputLength)
+    .fill()
+    .map((_, i) => min + i * dx);
+
   // A triangle at i causes the KDE to
   // - start increasing (+1) at i - width,
   // - change from increasing to decreasing (-2) at i, and
   // - stop decreasing (+1) at i + width.
   // So the result is a double prefix sum.
+  // The actual increase doesn't affect the point where the change
+  // happens, but rather the next one. Since y and dy perform inclusive
+  // prefix sums, the ysum values are offset by 1 above.
   let dy = 0;
   let y = 0;
-  const xs = Array(size)
-    .fill()
-    .map((_, i) => min + i * dx);
   const ys = xs.map((_, i) => {
     const ddy = ysum[i] - 2 * ysum[i + width] + ysum[i + 2 * width];
     dy += ddy;
