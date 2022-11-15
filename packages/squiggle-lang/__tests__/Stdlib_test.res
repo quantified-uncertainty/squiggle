9b8a045b903857e9ff5d7509a51b9a9ba7f5d7ff
Vladimir Serov <me@cab404.ru>
2022-11-15 13:18:22 +0400

    added distribution tests

--- packages/squiggle-lang/__tests__/Stdlib_test.res
+++ packages/squiggle-lang/__tests__/Stdlib_test.res
@@ -6,7 +6,7 @@ let makeTest = (~only=false, str, item1, item2) =>
     ? Only.test(str, () => expect(item1)->toEqual(item2))
     : test(str, () => expect(item1)->toEqual(item2))
 
-describe("Stdlib", () => {
+describe("Stdlib.Random", () => {
   makeTest(
     "Length of Random.sample",
     Stdlib.Random.sample([1.0, 2.0], {probs: [0.5, 0.5], size: 10})->E.A.length,
@@ -16,5 +16,75 @@ describe("Stdlib", () => {
     "Random.sample returns elements from input array (will fail with very slim probability)",
     Stdlib.Random.sample([1.0, 2.0], {probs: [0.5, 0.5], size: 10})->E.A.uniq->E.A.Floats.sort,
     [1.0, 2.0],
+  )
+})
+
+describe("Stdlib.Logistic", () => {
+  let mu = 0.0
+  let s = 2.0
+  let cdf = Stdlib.Logistic.cdf(mu, s)
+  let pdf = Stdlib.Logistic.pdf(mu, s)
+  // FIXME: this is ridiculous, fix the order in  the quantile function
+  let quantile = p => Stdlib.Logistic.quantile(p, mu, s)
+  let testRange = 10.0
+  let testSteps = 1000
+  let step = testRange *. 2.0 /. Belt.Int.toFloat(testSteps)
+  let iter = E.A.Floats.range(-.testRange, testRange, testSteps)
+
+  makeTest(
+    "CDF only grows",
+    iter->Js.Array2.map(cdf)->(arr => E.A.pairwise(arr, (a, b) => a <= b))->E.A.every(a => a),
+    true,
+  )
+  makeTest(
+    "CDF conforms to PDF",
+    iter
+    ->Js.Array2.map(a => (cdf(a), pdf(a)))
+    ->(arr => E.A.pairwise(arr, (a, b) => (a, b)))
+    ->E.A.every((((cdf, _pdf), (cdf', pdf'))) =>
+      Js.Math.abs_float(cdf' -. cdf -. pdf' *. step) < 0.0001
+    ),
+    true,
+  )
+  makeTest(
+    "Quantile is inverse of CDF",
+    iter
+    ->Js.Array2.map(p => (p, quantile(cdf(p))))
+    ->E.A.every(((p, pp)) => Js.Math.abs_float(p -. pp) < 0.00001),
+    true,
+  )
+})
+
+describe("Stdlib.Bernoulli", () => {
+  let p = 0.5
+  let cdf = Stdlib.Bernoulli.cdf(p)
+  let pmf = Stdlib.Bernoulli.pmf(p)
+  // FIXME: this is ridiculous, fix the order in  the quantile function
+  let quantile = prob => Stdlib.Bernoulli.quantile(prob, p)
+  let iter = [0.0, 1.0]
+  let step = 1.0
+
+  makeTest(
+    "CDF only grows",
+    iter->Js.Array2.map(cdf)->(arr => E.A.pairwise(arr, (a, b) => a <= b))->E.A.every(a => a),
+    true,
+  )
+  makeTest(
+    "CDF conforms to PMF",
+    iter
+    ->Js.Array2.map(a => (cdf(a), pmf(a)))
+    ->(arr => E.A.pairwise(arr, (a, b) => (a, b)))
+    ->E.A.every((((cdf, _pmf), (cdf', pmf'))) =>
+      Js.Math.abs_float(cdf' -. cdf -. pmf' *. step) < 0.0001
+    ),
+    true,
+  )
+
+  makeTest(
+    "Quantile is inverse of CDF",
+    iter
+    ->Js.Array2.map(p => (p, quantile(cdf(p))))
+    ->E.A.every(((p, pp)) => Js.Math.abs_float(p -. pp) < 0.00001),
+    true,
   )
 })
--- packages/squiggle-lang/src/js/math.ts
+++ packages/squiggle-lang/src/js/math.ts
@@ -4,12 +4,12 @@ type SampleArgs = {
 };
 
 /**
- * Searches for a first element in `arr` greater than `value` 
+ * Searches for a first element in `arr` greater than `value`
  * For more info:
  * https://github.com/quantified-uncertainty/squiggle/pull/1290#issuecomment-1302224197
-*/
+ */
 function binsearchFirstGreater(arr: number[], value: number): number {
-  let s = -1;             // Largest index known <=value
+  let s = -1; // Largest index known <=value
   let l = arr.length + 1; // Result is in (s,s+l]
   for (let h; (h = l >> 1) > 0; l -= h) {
     s += h * +(arr[s + h] <= value);
@@ -17,19 +17,19 @@ function binsearchFirstGreater(arr: number[], value: number): number {
   return s + 1;
 }
 
-export function randomSample(dist: number[], args: SampleArgs): number[] {
+export function random_sample(dist: number[], args: SampleArgs): number[] {
   const { probs, size } = args;
   let sample: number[] = Array(size);
 
-  let accum = 0
+  let accum = 0;
   let probPrefixSums = Array(probs.length);
   for (let index = 0; index < probs.length; index++) {
-    probPrefixSums[index] = (accum += probs[index])
+    probPrefixSums[index] = accum += probs[index];
   }
-  let sum = probPrefixSums[probPrefixSums.length - 1]
+  let sum = probPrefixSums[probPrefixSums.length - 1];
 
   for (let index = 0; index < size; index++) {
-    let selection = binsearchFirstGreater(probPrefixSums, Math.random() * sum)
+    let selection = binsearchFirstGreater(probPrefixSums, Math.random() * sum);
     sample[index] = dist[selection];
   }
   return sample;
@@ -39,7 +39,7 @@ export function randomSample(dist: number[], args: SampleArgs): number[] {
 export function factorial(n: number): number {
   let fct = 1;
   while (n > 0) {
-    fct *= n--
+    fct *= n--;
   }
   return fct;
 }
@@ -60,7 +60,7 @@ type DiscreteProbabilityDistribution<P> = Distribution<P> & {
   pmf(args: P, x: number): number;
 };
 
-let bernoulli: DiscreteProbabilityDistribution<number> = {
+export let bernoulli: DiscreteProbabilityDistribution<number> = {
   pmf: (p, x) => (x === 0 ? 1 - p : p),
   cdf: (p, x) => (x < 0 ? 0 : x >= 1 ? 1 : 1 - p),
   mean: (p) => p,
@@ -69,25 +69,30 @@ let bernoulli: DiscreteProbabilityDistribution<number> = {
   variance: (p) => p * (1 - p),
 };
 
-function sq(n: number): number { return n * n }
+function square(n: number): number {
+  return n * n;
+}
 
-let logistic: ContinuousProbabilityDistribution<{ mu: number; s: number }> = {
+export let logistic: ContinuousProbabilityDistribution<{
+  mu: number;
+  s: number;
+}> = {
   pdf: ({ mu, s }, x) => {
     if (s === 0) return mu === x ? +Infinity : 0;
-    let exp_delta = Math.exp(-((x - mu) / s));
+    let exp_delta = Math.pow(Math.E, -((x - mu) / s));
     return exp_delta / (s * square(1 + exp_delta));
   },
   cdf: ({ mu, s }, x) => {
     if (s === 0) return mu === x ? +Infinity : 0;
-    let exp_delta = Math.exp(-((x - mu) / s));
+    let exp_delta = Math.pow(Math.E, -((x - mu) / s));
     return 1 / (1 + exp_delta);
   },
   mean: ({ mu, s }) => mu,
-  stdev: ({ mu, s }) => Math.sqrt((sq(s) * sq(Math.PI)) / 3),
-  variance: ({ mu, s }) => (sq(s) * sq(Math.PI)) / 3,
+  stdev: ({ mu, s }) => Math.sqrt((square(s) * square(Math.PI)) / 3),
+  variance: ({ mu, s }) => (square(s) * square(Math.PI)) / 3,
   quantile: ({ mu, s }, p) => {
-    if (p === 0) return (s === 0) ? mu : +Infinity
-    return mu + s * Math.log(p / (1 - p))
+    if (p === 0) return s === 0 ? mu : +Infinity;
+    return mu + s * Math.log(p / (1 - p));
   },
 };
 
