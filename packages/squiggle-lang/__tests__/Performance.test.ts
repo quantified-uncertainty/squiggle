1a80ef9ffe46cf61fb62bdc4262b6999236c1c8f
Sam Nolan <13807753+Hazelfire@users.noreply.github.com>
2022-12-06 05:31:18 +0000

    Remove

--- packages/squiggle-lang/__tests__/Performance.test.ts
+++ packages/squiggle-lang/__tests__/Performance.test.ts
@@ -3,10 +3,6 @@ import malariaConsortiumCells from "./malaria_consortium.json";
 
 test("Renders Malaria Consortium Notebook in reasonable amount of time", async () => {
   const project = SqProject.create();
-  /*  project.setEnvironment({
-    sampleCount: 1000,
-    xyPointLength: 1000,
-  });*/
   malariaConsortiumCells.forEach((cell) => {
     project.setSource(cell.id, cell.source);
     project.setContinues(cell.id, cell.continues);
