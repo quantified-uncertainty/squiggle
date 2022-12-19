427885442b83d0934f3045ce43ec417aaf670e03
Sam Nolan <13807753+Hazelfire@users.noreply.github.com>
2022-12-19 05:56:22 +0000

    Change output of Performance test

--- packages/squiggle-lang/__tests__/Performance.test.ts
+++ packages/squiggle-lang/__tests__/Performance.test.ts
@@ -1,11 +1,26 @@
 import { SqProject } from "../src";
 import malariaConsortiumCells from "./malaria_consortium.json";
+import { Session } from 'inspector';
+import fs from 'node:fs';
 
-test("Renders Malaria Consortium Notebook in reasonable amount of time", async () => {
-  const project = SqProject.create();
-  malariaConsortiumCells.forEach((cell) => {
-    project.setSource(cell.id, cell.source);
-    project.setContinues(cell.id, cell.continues);
-  });
-  project.runAll();
+test("Renders Malaria Consortium Notebook in reasonable amount of time", () => {
+  new Promise((resolve, reject) => {
+    const session = new Session();
+    session.connect();
+    
+    session.post('Profiler.enable', () => {
+      session.post('Profiler.start', () => {
+        const project = SqProject.create();
+        malariaConsortiumCells.forEach((cell) => {
+          project.setSource(cell.id, cell.source);
+          project.setContinues(cell.id, cell.continues);
+        });
+        project.runAll();
+        session.post('Profiler.stop', (err, {profile}) => {
+          fs.writeFileSync('./malaria_consortium.cpuprofile', JSON.stringify(profile));
+          resolve(undefined)
+        });
+      });
+    });
+  })
 });
