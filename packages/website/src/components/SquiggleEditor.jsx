857e26de7fa78e8d990ed46315730c4f43c2a9e0
Sam Nolan <samnolan555@gmail.com>
2023-03-02 14:39:34 +1100

    Remove react-ace, move website to SSR

--- packages/website/src/components/SquiggleEditor.jsx
+++ packages/website/src/components/SquiggleEditor.jsx
@@ -1,26 +1,4 @@
-import BrowserOnly from "@docusaurus/BrowserOnly";
-import { FallbackSpinner } from "./FallbackSpinner";
-
-export function SquiggleEditor(props) {
-  return (
-    <BrowserOnly fallback={<FallbackSpinner height={292} />}>
-      {() => {
-        const LibComponent =
-          require("@quri/squiggle-components").SquiggleEditor;
-        return <LibComponent {...props} />;
-      }}
-    </BrowserOnly>
-  );
-}
-
-export function SquiggleEditorWithImportedBindings(props) {
-  return (
-    <BrowserOnly fallback={<FallbackSpinner height={292} />}>
-      {() => {
-        const LibComponent =
-          require("@quri/squiggle-components").SquiggleEditorWithImportedBindings;
-        return <LibComponent {...props} />;
-      }}
-    </BrowserOnly>
-  );
-}
+export {
+  SquiggleEditor,
+  SquiggleEditorWithImportedBindings,
+} from "@quri/squiggle-components";
