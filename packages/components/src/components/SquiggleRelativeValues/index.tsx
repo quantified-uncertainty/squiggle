feef5d146056fbb43eafa3162fefa152eab0b893
Vyacheslav Matyukhin <me@berekuk.ru>
2023-03-09 17:43:37 -0600

    histograms and other UI improvements

--- packages/components/src/components/SquiggleRelativeValues/index.tsx
+++ packages/components/src/components/SquiggleRelativeValues/index.tsx
@@ -1,13 +1,26 @@
-import { SqLambda, SqProject } from "@quri/squiggle-lang";
-import { SqStringValue, SqValue } from "@quri/squiggle-lang/src/public/SqValue";
+import {
+  Env,
+  SqDistribution,
+  SqDistributionTag,
+  SqLambda,
+  SqProject,
+} from "@quri/squiggle-lang";
+import { SqValue } from "@quri/squiggle-lang/src/public/SqValue";
 import React, { useEffect, useMemo, useState } from "react";
+import { NumberShower } from "../NumberShower";
 import { SquiggleViewer } from "../SquiggleViewer";
+import { Histogram } from "./Histogram";
+
+type Choice = {
+  id: string;
+  name: string;
+};
 
 const useRelativeValues = (code: string) => {
   const project = useMemo(() => SqProject.create(), []);
 
   const [error, setError] = useState("");
-  const [choices, setChoices] = useState<string[]>([]);
+  const [choices, setChoices] = useState<Choice[]>([]);
   const [fn, setFn] = useState<SqLambda | undefined>();
 
   useEffect(() => {
@@ -53,56 +66,146 @@ const useRelativeValues = (code: string) => {
     }
     const choiceValues = choicesValue.value.getValues();
 
-    setChoices(
-      choiceValues
-        // TODO: we should report non-string values as errors instead
-        .filter((value): value is SqStringValue => value.tag === "String")
-        .map((value) => value.value)
-    );
+    const choices: Choice[] = [];
+    for (let choiceValue of choiceValues) {
+      if (choiceValue.tag !== "Record") {
+        setError(`choice should be a record, got: ${choiceValue.tag}`);
+        return;
+      }
+      let id: string | undefined, name: string | undefined;
+      for (let [k, v] of choiceValue.value.entries()) {
+        if (k === "id") {
+          if (v.tag !== "String") {
+            setError("expected string for id");
+            return;
+          }
+          id = v.value;
+        }
+        if (k === "name") {
+          if (v.tag !== "String") {
+            setError("expected string for name");
+            return;
+          }
+          name = v.value;
+        }
+      }
+
+      if (id === undefined) {
+        setError("id field not present");
+        return;
+      }
+      if (name === undefined) {
+        setError("name field not present");
+        return;
+      }
+
+      choices.push({
+        id,
+        name,
+      });
+    }
+
+    setChoices(choices);
   }, [project, code]);
 
-  return { error, choices, fn };
+  return { error, choices, fn, project };
+};
+
+const CellError: React.FC<{ error: string }> = ({ error }) => {
+  // TODO - truncate
+  return <div className="text-red-500 text-xs">{error}</div>;
+};
+
+const Cell: React.FC<{ dist: SqDistribution; env: Env }> = ({ dist, env }) => {
+  if (dist.tag !== SqDistributionTag.SampleSet) {
+    // TODO - convert automatically?
+    return <CellError error="Expected sample set" />;
+  }
+
+  const median = dist.inv(env, 0.5);
+
+  const samples = [...dist.value().samples].sort((a, b) => a - b);
+  return (
+    <div className="h-8 pt-[1px] relative">
+      <div className="absolute top-0 inset-x-0 text-center">
+        {median.ok ? <NumberShower number={median.value} /> : null}
+      </div>
+      <Histogram data={samples} />
+    </div>
+  );
 };
 
 type Props = {
   code: string;
+  showDebugViewer?: boolean;
 };
 
-export const SquiggleRelativeValues: React.FC<Props> = ({ code }) => {
-  const { error, choices, fn } = useRelativeValues(code);
+export const SquiggleRelativeValues: React.FC<Props> = ({
+  code,
+  showDebugViewer,
+}) => {
+  const { error, choices, fn, project } = useRelativeValues(code);
 
-  const renderCompare = (choice1: string, choice2: string) => {
+  const renderCompare = (choice1: Choice, choice2: Choice) => {
     if (!fn) {
       return null;
     }
-    const result = fn.call([choice1, choice2]);
-    return result.value.toString();
-    // return <SquiggleViewer result={result} />;
+    const result = fn.call([choice1.id, choice2.id]);
+    // return result.value.toString();
+    if (!result.ok) {
+      return <CellError error={result.value.toString()} />;
+    }
+
+    const value = result.value;
+    if (value.tag !== "Dist") {
+      return <CellError error="Expected dist" />;
+    }
+
+    return (
+      <div>
+        {showDebugViewer && (
+          <SquiggleViewer result={result} enableLocalSettings={true} />
+        )}
+        <Cell dist={value.value} env={project.getEnvironment()} />
+      </div>
+    );
   };
 
   return (
     <div>
       {error && <pre className="text-red-700">{error}</pre>}
-      <table className="text-xs">
+      <table className="text-xs table-fixed">
         <thead>
-          <th />
-          {choices.map((choice2) => (
-            <th key={choice2} className="border border-gray-200 p-1">
-              {choice2}
-            </th>
-          ))}
+          <tr>
+            <th />
+            {choices.map((choice) => (
+              <th
+                key={choice.id}
+                className="border border-gray-200 p-1 bg-gray-50"
+              >
+                <div className="w-40">{choice.name}</div>
+              </th>
+            ))}
+          </tr>
         </thead>
         <tbody>
-          {choices.map((choice1) => (
-            <tr key={choice1}>
-              <td key={0} className="font-bold border border-gray-200 p-1">
-                {choice1}
+          {choices.map((choice1, i1) => (
+            <tr key={choice1.id}>
+              <td
+                key={0}
+                className="font-bold border border-gray-200 bg-gray-50 p-1"
+              >
+                {choice1.name}
               </td>
-              {choices.map((choice2) => (
-                <td key={choice2} className="border border-gray-200 p-1">
-                  {renderCompare(choice1, choice2)}
-                </td>
-              ))}
+              {choices.map((choice2, i2) =>
+                i2 < i1 ? (
+                  <td key={choice2.id} className="border border-gray-200 p-0">
+                    {renderCompare(choice1, choice2)}
+                  </td>
+                ) : (
+                  <td key={choice2.id} className="bg-gray-200" />
+                )
+              )}
             </tr>
           ))}
         </tbody>
