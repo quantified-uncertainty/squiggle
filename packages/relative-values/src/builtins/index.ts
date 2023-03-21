93888973a3a6c4db04242c1a5eefc0356165154b
Ozzie Gooen <ozzieagooen@gmail.com>
2023-03-20 18:57:01 -0600

    Added simple example of lots of QURI posts

--- packages/relative-values/src/builtins/index.ts
+++ packages/relative-values/src/builtins/index.ts
@@ -1,8 +1,10 @@
 import { InterfaceWithModels } from "@/types";
 import { getQuriBuiltin } from "./quri";
 import { getQuriSoftwareBuiltin } from "./quri-software";
+import { getQuriPosts } from "./quri-posts";
 
 export const allInterfaces: InterfaceWithModels[] = [
   getQuriBuiltin(),
   getQuriSoftwareBuiltin(),
+  getQuriPosts()
 ];
