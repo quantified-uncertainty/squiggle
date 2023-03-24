4f06462171711940f0a89bf931e0cd8163b438b5
NunoSempere <nuno.sempere@protonmail.com>
2023-03-24 15:43:31 -0600

    feat: add relative values for some "things you should buy"

---
+++ packages/relative-values/src/builtins/templates/make_ts_file.sh
@@ -0,0 +1,2 @@
+sed '/^SQUIGGLE_CODE/ r /home/loki/Documents/core/ea/fresh/QURI/fresh/relative-values/attempt_4_buy/buy.squiggle' /home/loki/Documents/core/software/fresh/js/squiggle/squiggle-buy-relative/packages/relative-values/src/builtins/templates/some-things-you-should-buy.template > /home/loki/Documents/core/software/fresh/js/squiggle/squiggle-buy-relative/packages/relative-values/src/builtins/things-you-should-buy.ts
+
