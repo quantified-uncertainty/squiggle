f003c2dfa4932fd4fa33eac827fa55af2270c0d7
Vyacheslav Matyukhin <me@berekuk.ru>
2023-04-03 16:51:41 -0600

    plots next to each other

--- packages/relative-values/src/components/View/PlotView/index.tsx
+++ packages/relative-values/src/components/View/PlotView/index.tsx
@@ -1,33 +1,37 @@
-import { StyledTab } from "@/components/ui/StyledTab";
 import { ModelEvaluator } from "@/values/ModelEvaluator";
-import { SqLambda } from "@quri/squiggle-lang";
-import { FC } from "react";
+import clsx from "clsx";
+import { FC, PropsWithChildren } from "react";
 import { ClusterFilter } from "../ClusterFilter";
 import { ForcePlot } from "./ForcePlot";
 import { ValueAndUncertaintyPlot } from "./ValueAndUncertaintyPlot";
+
+const Section: FC<PropsWithChildren<{ title: string; border?: boolean }>> = ({
+  title,
+  border,
+  children,
+}) => (
+  <div className="flex flex-col gap-2 items-center">
+    <header className="text-slate-800 font-bold">{title}</header>
+    <div className={clsx(border && "border border-slate-100 p-1")}>
+      {children}
+    </div>
+  </div>
+);
 
 export const PlotView: FC<{
   model: ModelEvaluator;
 }> = ({ model }) => {
   return (
-    <div className="flex gap-8 max-w-6xl mx-auto">
-      <div>
-        <StyledTab.Group>
-          <StyledTab.List>
-            <StyledTab name="Value and Uncertainty" icon={() => <div />} />
-            <StyledTab name="Force" icon={() => <div />} />
-          </StyledTab.List>
-          <StyledTab.Panels>
-            <StyledTab.Panel>
-              <ValueAndUncertaintyPlot model={model} />
-            </StyledTab.Panel>
-            <StyledTab.Panel>
-              <ForcePlot model={model} />
-            </StyledTab.Panel>
-          </StyledTab.Panels>
-        </StyledTab.Group>
-      </div>
-      <ClusterFilter axis="rows" />
+    <div className="flex gap-2 max-w-6xl mx-auto">
+      <Section title="Value and Uncertainty" border>
+        <ValueAndUncertaintyPlot model={model} />
+      </Section>
+      <Section title="Force" border>
+        <ForcePlot model={model} />
+      </Section>
+      <Section title="Clusters">
+        <ClusterFilter axis="rows" />
+      </Section>
     </div>
   );
 };
