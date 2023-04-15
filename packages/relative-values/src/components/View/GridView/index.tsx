f530233e1494e7edf08b26bb68c1e64d370321c4
Ozzie Gooen <ozzieagooen@gmail.com>
2023-04-14 20:50:04 -0700

    First attempt at cluster-grid

--- packages/relative-values/src/components/View/GridView/index.tsx
+++ packages/relative-values/src/components/View/GridView/index.tsx
@@ -1,42 +1,62 @@
 import { Item } from "@/types";
 import { ModelEvaluator } from "@/values/ModelEvaluator";
-import { FC, Fragment, useCallback, useMemo } from "react";
+import { FC, Fragment, useCallback, useMemo, useState } from "react";
 import { useSelectedInterface } from "../../Interface/InterfaceProvider";
 import { DropdownButton } from "../../ui/DropdownButton";
 import { Header } from "../Header";
 import { useFilteredItems, useSortedItems } from "../hooks";
 import { RelativeCell } from "../RelativeCell";
-import { useViewContext } from "../ViewProvider";
+import { useViewContext, AxisConfig } from "../ViewProvider";
 import { AxisMenu } from "./AxisMenu";
 import { GridModeControls } from "./GridModeControls";
 import { CellBox } from "../CellBox";
+import { RelativeValue } from "@/values/types";
+import _ from "lodash";
+import { DistCell } from "../RelativeCell/DistCell";
+import { Set } from "immutable";
 
-export const GridView: FC<{
+export const GridView2: FC<{
   model: ModelEvaluator;
-}> = ({ model }) => {
+  rowFilter: string | undefined;
+  columnFilter: string | undefined;
+}> = ({ model, rowFilter, columnFilter }) => {
   const { axisConfig, gridMode } = useViewContext();
   const {
     catalog: { items },
   } = useSelectedInterface();
 
+  let axisConfig2: {
+    rows: AxisConfig;
+    columns: AxisConfig;
+  } = {
+    rows: {
+      filter: { selectedClusters: Set([rowFilter || ""]) },
+      sort: axisConfig.rows.sort,
+    },
+    columns: {
+      filter: { selectedClusters: Set([columnFilter || ""]) },
+      sort: axisConfig.columns.sort,
+    },
+  };
+
   const filteredRowItems = useFilteredItems({
     items: items,
-    config: axisConfig.rows,
+    config: axisConfig2.rows,
   });
   const filteredColumnItems = useFilteredItems({
     items: items,
-    config: axisConfig.columns,
+    config: axisConfig2.columns,
   });
 
   const rowItems = useSortedItems({
     items: filteredRowItems,
-    config: axisConfig.rows,
+    config: axisConfig2.rows,
     model: model,
     otherDimensionItems: filteredColumnItems,
   });
   const columnItems = useSortedItems({
     items: filteredColumnItems,
-    config: axisConfig.columns,
+    config: axisConfig2.columns,
     model: model,
     otherDimensionItems: filteredRowItems,
   });
@@ -112,6 +132,91 @@ export const GridView: FC<{
           </Fragment>
         ))}
       </div>
+    </div>
+  );
+};
+
+export const GridView: FC<{
+  model: ModelEvaluator;
+}> = ({ model }) => {
+  const {
+    catalog: { items, clusters },
+  } = useSelectedInterface();
+
+  const [selectedBox, setSelectedBox] = useState<undefined | string[]>(
+    undefined
+  );
+
+  const clusterItems: Item[] = Object.keys(clusters).map((id) => {
+    const cluster = clusters[id];
+    return {
+      id,
+      name: `${cluster.name} - ${
+        items.filter((item) => item.clusterId === id).length
+      }`,
+    };
+  });
+
+  const combinationItems = (rowItem: Item, columnItem: Item): RelativeValue => {
+    const rowItems = items.filter((item) => item.clusterId === rowItem.id);
+    const columnItems = items.filter(
+      (item) => item.clusterId === columnItem.id
+    );
+    const combinations = rowItems
+      .flatMap((row) =>
+        columnItems.flatMap((column) => model.compare(row.id, column.id))
+      )
+      .flatMap((x) => (x.ok ? [x.value] : []));
+    return {
+      mean: _.mean(combinations.map((r) => r.mean)),
+      median: _.mean(combinations.map((r) => r.median)),
+      min: _.mean(combinations.map((r) => r.min)),
+      max: _.mean(combinations.map((r) => r.max)),
+      uncertainty: _.mean(combinations.map((r) => r.uncertainty)),
+    };
+  };
+
+  //It seems nicer, at this point, to just specify that its p25 and p75
+  return (
+    <div>
+      <div
+        className="grid relative"
+        style={{
+          gridTemplateColumns: `repeat(${clusterItems.length + 1}, 200px)`,
+        }}
+      >
+        <div className="sticky bg-white top-0 left-0 z-20" />
+        {clusterItems.map((item) => (
+          <CellBox header key={item.id}>
+            <Header key={item.id} item={item} />
+          </CellBox>
+        ))}
+        {clusterItems.map((rowItem) => (
+          <Fragment key={rowItem.id}>
+            <CellBox header>
+              <Header key={0} item={rowItem} />
+            </CellBox>
+            {clusterItems.map((columnItem) => (
+              <div
+                key={rowItem.id}
+                onClick={() => setSelectedBox([rowItem.id, columnItem.id])}
+                className="pointer"
+              >
+                <DistCell
+                  item={combinationItems(rowItem, columnItem)}
+                  uncertaintyPercentiles={[2, 30]}
+                />
+              </div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+
+      <GridView2
+        model={model}
+        rowFilter={selectedBox && selectedBox[0]}
+        columnFilter={selectedBox && selectedBox[1]}
+      />
     </div>
   );
 };
