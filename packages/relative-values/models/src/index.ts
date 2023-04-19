65c818ca6ad67c507ba495ba74819f740bbd3964
Ozzie Gooen <ozzieagooen@gmail.com>
2023-04-19 16:04:45 -0700

    Remove interfaces for development

--- packages/relative-values/models/src/index.ts
+++ packages/relative-values/models/src/index.ts
@@ -9,10 +9,6 @@ import { Map } from "immutable";
 
 const items = [
   { catalog: quri.catalog, models: quri.models },
-  { catalog: software.catalog, models: software.models },
-  { catalog: posts.catalog, models: posts.models },
-  { catalog: health.catalog, models: health.models },
-  { catalog: purchases.catalog, models: purchases.models },
   { catalog: macrovariables.catalog, models: macrovariables.models },
 ];
 
