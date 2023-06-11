3bc6a481d7a8dd34765073198bc8871da80c3e29
Vyacheslav Matyukhin <me@berekuk.ru>
2023-06-11 14:32:23 -0600

    NumberFormField doesn't allow letters and uses type='text'

--- packages/ui/src/forms/fields/NumberFormField.tsx
+++ packages/ui/src/forms/fields/NumberFormField.tsx
@@ -1,6 +1,6 @@
 import { FieldPath, FieldValues } from "react-hook-form";
 
-import { FormField } from "../common/FormField.js";
+import { ControlledFormField } from "../common/ControlledFormField.js";
 import { CommonNumberFieldProps } from "../common/types.js";
 import { StyledInput, type StyledInputProps } from "../styled/StyledInput.js";
 
@@ -13,16 +13,38 @@ export function NumberFormField<
   ...fieldProps
 }: CommonNumberFieldProps<TValues, TName> &
   Pick<StyledInputProps, "size" | "placeholder">) {
+  // `valueAsNumber` in react-hook-form doesn't play well with zod, and `setValueAs` is problematic too, so we have to use controlled component instead.
+  // Also, this lets us to disallow non-numerical characters.
+  // See also: https://github.com/orgs/react-hook-form/discussions/6980, https://github.com/orgs/react-hook-form/discussions/7128
   return (
-    <FormField {...fieldProps} rules={{ ...rules, valueAsNumber: true }}>
-      {(inputProps) => (
-        <StyledInput
-          type="number"
-          {...inputProps}
-          placeholder={placeholder}
-          autoComplete="off" // TODO - make customizable?
-        />
-      )}
-    </FormField>
+    <ControlledFormField {...fieldProps} rules={rules}>
+      {({ onChange, onBlur, value }) => {
+        return (
+          <StyledInput
+            // type="number" is broken in Firefox.
+            // See also: https://news.ycombinator.com/item?id=32852643
+            type="text"
+            onBlur={onBlur}
+            value={value === null ? "" : value}
+            onChange={(e) => {
+              const str = e.target.value;
+              if (str.match(/^\s*$/)) {
+                onChange(null);
+              } else if (str === "-") {
+                onChange(str); // this won't pass zod validation, but it will affect `value` field
+              } else {
+                const newValue = +e.target.value;
+                if (Number.isNaN(newValue)) {
+                  return; // don't allow edit
+                }
+                onChange(newValue);
+              }
+            }}
+            placeholder={placeholder}
+            autoComplete="off" // TODO - make customizable?
+          />
+        );
+      }}
+    </ControlledFormField>
   );
 }
