7033287fd3188e3c4d5e4f3b603d8290533c0ca7
Vyacheslav Matyukhin <me@berekuk.ru>
2023-06-28 14:07:53 -0600

    hide footer on model pages

--- packages/hub/src/components/layout/RootLayout/PageFooter.tsx
+++ packages/hub/src/components/layout/RootLayout/PageFooter.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { isModelRoute } from "@/routes";
+import { usePathname } from "next/navigation";
 import { FC } from "react";
 
-export const PageFooter: FC = () => (
-  <div className="p-8 border-t border-t-slate-200 bg-slate-100">
-    <div className="text-sm text-slate-500">
-      By{" "}
-      <a
-        href="https://quantifieduncertainty.org"
-        className="text-blue-500 hover:underline"
-      >
-        QURI
-      </a>
+export const PageFooter: FC = () => {
+  const pathname = usePathname();
+  if (isModelRoute(pathname)) {
+    return null; // hide footer on model pages
+  }
+
+  return (
+    <div className="p-8 border-t border-t-slate-200 bg-slate-100">
+      <div className="text-sm text-slate-500">
+        By{" "}
+        <a
+          href="https://quantifieduncertainty.org"
+          className="text-blue-500 hover:underline"
+        >
+          QURI
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
