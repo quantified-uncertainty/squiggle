d59603ec99dbeff44ea253865b279b9e4ea90543
Vyacheslav Matyukhin <me@berekuk.ru>
2023-08-21 14:43:24 -0600

    enable strict mode, refactor types

--- packages/website/src/components/PlaygroundPage/VersionedPlayground.tsx
+++ packages/website/src/components/PlaygroundPage/VersionedPlayground.tsx
@@ -1,5 +1,6 @@
-import { ComponentType, FC } from "react";
 import dynamic from "next/dynamic";
+import { FC } from "react";
+
 import { useAvailableHeight } from "../../utils/useAvailableHeight";
 
 // Imported non-dynamically as a demo of version pinning.
@@ -13,6 +14,16 @@ export type Version = (typeof versions)[number];
 
 export const defaultVersion: Version = "0.8.4";
 
+// Note: typing this with `{ [k in Version]: ComponentType<CommonProps> }` won't work because of contravariance issues.
+// Instead, we pass all props explicitly to the playground component when it's instantiated to check that all props are compatible.
+const playgroundByVersion = {
+  "0.8.4": Playground084,
+  "0.8.5-dev": dynamic(() =>
+    import("@quri/squiggle-components").then((mod) => mod.SquigglePlayground)
+  ),
+};
+
+// We expect all playground components to be a subtype of this type.
 type CommonProps = {
   defaultCode?: string;
   distributionChartSettings?: { showSummary?: boolean }; // simplified
@@ -24,16 +35,6 @@ type CommonProps = {
     distributionChartSettings: { showSummary: boolean };
   }) => void;
   height?: string | number;
-};
-
-const playgroundByVersion: {
-  // checking that all versions are compatible with props that we plan to pass
-  [k in Version]: ComponentType<CommonProps>;
-} = {
-  "0.8.4": Playground084,
-  "0.8.5-dev": dynamic(() =>
-    import("@quri/squiggle-components").then((mod) => mod.SquigglePlayground)
-  ),
 };
 
 type Props = Omit<CommonProps, "height"> & { version: Version };
@@ -49,7 +50,16 @@ export const VersionedPlayground: FC<Props> = ({ version, ...props }) => {
   return (
     <div style={{ minHeight: height }} ref={ref}>
       {Playground ? (
-        <Playground {...props} height={height} />
+        <Playground
+          // Listing all props for better type safety, instead of using `{...props}`.
+          // Playground props shape can change in the future and this allows us to catch those cases early.
+          defaultCode={props.defaultCode}
+          distributionChartSettings={props.distributionChartSettings}
+          renderExtraControls={props.renderExtraControls}
+          onCodeChange={props.onCodeChange}
+          onSettingsChange={props.onSettingsChange}
+          height={height}
+        />
       ) : (
         <div>Failed to load playground components for version {version}</div>
       )}
