319890524c3ee14e207e3012da5799edea0e9e5b
Vyacheslav Matyukhin <me@berekuk.ru>
2023-08-23 19:19:22 -0600

    eslint for versioned-playground

---
+++ packages/versioned-playground/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
