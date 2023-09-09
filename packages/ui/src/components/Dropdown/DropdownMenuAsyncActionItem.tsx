f66ea34db2e8c277d59c06109cad5a0470ea6529
Vyacheslav Matyukhin <me@berekuk.ru>
2023-09-08 21:30:02 -0600

    even more "use client"

--- packages/ui/src/components/Dropdown/DropdownMenuAsyncActionItem.tsx
+++ packages/ui/src/components/Dropdown/DropdownMenuAsyncActionItem.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC, useState } from "react";
 
 import { IconProps } from "../../icons/Icon.js";
--- packages/ui/src/components/TailwindProvider.tsx
+++ packages/ui/src/components/TailwindProvider.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC, PropsWithChildren, createContext } from "react";
 
 type TailwindContextShape = {
