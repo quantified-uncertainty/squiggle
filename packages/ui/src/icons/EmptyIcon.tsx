881cce8a89cadab866c6dfd37e2d73477e25c3cf
Vyacheslav Matyukhin <me@berekuk.ru>
2023-09-09 14:57:41 -0600

    minor cleanups

--- packages/ui/src/icons/EmptyIcon.tsx
+++ packages/ui/src/icons/EmptyIcon.tsx
@@ -1,7 +1,4 @@
 import { FC } from "react";
 import { Icon, IconProps } from "./Icon.js";
 
-export const EmptyIcon: FC<IconProps> = (props) => (
-  // circle-stack
-  <Icon {...props} />
-);
+export const EmptyIcon: FC<IconProps> = (props) => <Icon {...props} />;
