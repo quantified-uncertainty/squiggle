31576d88014fe5979fed9800e46bc71de3ea0397
Ozzie Gooen <ozzieagooen@gmail.com>
2023-11-12 13:59:28 -0800

    Added extra calculator story

--- packages/components/src/stories/SquiggleChart/Calculator.stories.tsx
+++ packages/components/src/stories/SquiggleChart/Calculator.stories.tsx
@@ -26,6 +26,32 @@ export const Basic: Story = {
           Input.select({name: "Variable3", default: "alice", options: ["alice", "charles", "bob", "bill", "maven", "billy", "samantha", "becky"]})
         ],
         sampleCount: 1000,
+        autorun: true 
+      }
+    )
+      
+`,
+  },
+};
+
+export const WithoutAutorun: Story = {
+  args: {
+    code: `
+    f(a, b, c, d) = {|f| normal(f*10+b, 1)}
+    a = "A longer description of the calculator goes here...
+    "
+    Calculator.make(
+      {
+        fn: f,
+        title: "My Calculator",
+        description: a,
+        inputs: [
+          Input.checkbox({name: "VariableCheckbox", description: "This is a long name", default: false}),
+          Input.textArea({name: "Variable2", description: "This is a long name", default: "2 to 40"}),
+          Input.text({name: "Variable1", description: "This is a very long description This is a very long description This is a very long description This is a very long description This is a very long description", default: 1}),
+          Input.select({name: "Variable3", default: "alice", options: ["alice", "charles", "bob", "bill", "maven", "billy", "samantha", "becky"]})
+        ],
+        sampleCount: 1000,
         autorun: false
       }
     )
