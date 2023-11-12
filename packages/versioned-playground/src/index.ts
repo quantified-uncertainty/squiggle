5cb931f03a319443f165f42edd778a1c62774a23
Vyacheslav Matyukhin <me@berekuk.ru>
2023-11-12 02:39:52 -0600

    use client fixes

--- packages/versioned-playground/src/index.ts
+++ packages/versioned-playground/src/index.ts
@@ -1,12 +1,13 @@
-export { VersionedSquigglePlayground } from "./VersionedSquigglePlayground.js";
 export { SquigglePlaygroundVersionPicker } from "./SquigglePlaygroundVersionPicker.js";
 export { SquiggleVersionShower } from "./SquiggleVersionShower.js";
+export { VersionedSquigglePlayground } from "./VersionedSquigglePlayground.js";
 
 export {
-  squiggleVersions,
-  type SquiggleVersion,
-  defaultSquiggleVersion,
   checkSquiggleVersion,
-  useValidSquiggleVersion,
+  defaultSquiggleVersion,
+  squiggleVersions,
   versionSupportsDropdownMenu,
+  type SquiggleVersion,
 } from "./versions.js";
+
+export { useValidSquiggleVersion } from "./hooks.js";
