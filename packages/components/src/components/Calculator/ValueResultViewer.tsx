d9432751bb4023befe46a1177a55d4c5986ab933
Ozzie Gooen <ozzieagooen@gmail.com>
2023-11-13 15:24:16 -0800

    Trying to fix Calculator, not succeeding

--- packages/components/src/components/Calculator/ValueResultViewer.tsx
+++ packages/components/src/components/Calculator/ValueResultViewer.tsx
@@ -1,19 +1,34 @@
-import { FC } from "react";
+import { FC, memo, useMemo } from "react";
 
-import { valueHasContext } from "../../lib/utility.js";
+import { SqValueWithContext, valueHasContext } from "../../lib/utility.js";
 import { PlaygroundSettings } from "../PlaygroundSettings.js";
-import { getSqValueWidget } from "../SquiggleViewer/getSqValueWidget.js";
+// import { getSqValueWidget } from "../SquiggleViewer/getSqValueWidget.js";
+import { GetSqValueWidget } from "../SquiggleViewer/getSqValueWidget2.js";
 import { SqValueResult } from "./types.js";
 
+// const Memoized = memo(function GetSqValueWidget({
+//   value,
+//   settings,
+// }: {
+//   value: SqValueWithContext;
+//   settings: PlaygroundSettings;
+// }) {
+//   return <GetSqValueWidget value={value} settings={settings} />;
+// });
+
+const Memoized = memo(GetSqValueWidget);
 // Unlike ValueViewer/ValueWithContextViewer, this just renders the raw widget; TODO - better name?
 export const ValueResultViewer: FC<{
   result: SqValueResult;
   settings: PlaygroundSettings;
 }> = ({ result, settings }) => {
-  if (result.ok) {
-    const value = result.value;
+  const memoizedValue = useMemo(() => result, [result]);
+  const memoizedSettings = useMemo(() => settings, [settings]);
+  if (memoizedValue.ok) {
+    const value = memoizedValue.value;
+    console.log("HI", value);
     if (valueHasContext(value)) {
-      return getSqValueWidget(value).render(settings);
+      return <Memoized value={value} settings={memoizedSettings} />;
     } else {
       return value.toString();
     }
