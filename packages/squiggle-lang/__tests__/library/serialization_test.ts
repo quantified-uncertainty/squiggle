dc47a5f6ec2d4ad3192cda30940ae0b041e38eb3
Ozzie Gooen <ozzieagooen@gmail.com>
2023-12-30 12:21:43 -0800

    Some simple JSON tests

--- packages/squiggle-lang/__tests__/library/serialization_test.ts
+++
@@ -1,7 +0,0 @@
-import { testEvalToBe } from "../helpers/reducerHelpers.js";
-
-describe("JSON", () => {
-  testEvalToBe("Danger.json(1)", "1");
-  testEvalToBe("Danger.json([1,2,3])", "[1,2,3]");
-  testEvalToBe("Danger.json({foo: 'bar'})", '{foo: "bar"}');
-});
