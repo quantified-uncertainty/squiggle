9081998fab77c8637d7455606aa046c0c73365b8
Vyacheslav Matyukhin <me@berekuk.ru>
2023-12-30 16:48:09 -0600

    remove import assertion

--- packages/components/src/lib/MarkdownViewer.tsx
+++ packages/components/src/lib/MarkdownViewer.tsx
@@ -13,7 +13,10 @@ import {
 import { Node, Parent } from "unist";
 import { visitParents } from "unist-util-visit-parents";
 
-import squiggleGrammar from "@quri/squiggle-textmate-grammar/dist/squiggle.tmLanguage.json" assert { type: "json" };
+// Import assertion here would be nice, but storybook doesn't support "assert" syntax yet, only "with" syntax,
+// and our import sorter auto-replaces with the newer "assert" syntax.
+// This will be fixed in storybook eventually, https://github.com/storybookjs/storybook/issues/23599
+import squiggleGrammar from "@quri/squiggle-textmate-grammar/dist/squiggle.tmLanguage.json";
 
 type SupportedLanguage = BundledLanguage | "squiggle";
 
