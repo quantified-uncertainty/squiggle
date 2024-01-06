1f106a6cad629158f5a63b15d3dbe1260ce5009c
Ozzie Gooen <ozzieagooen@gmail.com>
2024-01-06 13:41:21 -0800

    experimental-list-display

--- packages/components/src/widgets/ArrayWidget.tsx
+++ packages/components/src/widgets/ArrayWidget.tsx
@@ -4,17 +4,23 @@ import { ValueViewer } from "../components/SquiggleViewer/ValueViewer.js";
 import { widgetRegistry } from "./registry.js";
 import { SqTypeWithCount } from "./SqTypeWithCount.js";
 
+const isVisual = false;
+
 widgetRegistry.register("Array", {
-  heading: (value) => `List(${value.value.getValues().length})`,
-  Preview: (value) => (
-    <SqTypeWithCount type="[]" count={value.value.getValues().length} />
-  ),
+  heading: (value) =>
+    isVisual ? `List(${value.value.getValues().length})` : "",
+  Preview: (value) =>
+    isVisual ? (
+      <SqTypeWithCount type="[]" count={value.value.getValues().length} />
+    ) : (
+      ""
+    ),
   Chart: (value) => {
     const values = useMemo(() => value.value.getValues(), [value]);
     return (
       <div className="space-y-1 pt-0.5 mt-0.5">
         {values.map((r, i) => (
-          <ValueViewer parentValue={value} key={i} value={r} />
+          <ValueViewer parentValue={value} key={i} value={r} header={"hide"} />
         ))}
       </div>
     );
