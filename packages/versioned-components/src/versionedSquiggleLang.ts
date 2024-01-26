45da56534b558ea82915cad5fec00f8fb12ec17e
Vyacheslav Matyukhin <me@berekuk.ru>
2024-01-26 17:06:16 -0600

    more patch-js preparations

--- packages/versioned-components/src/versionedSquiggleLang.ts
+++ packages/versioned-components/src/versionedSquiggleLang.ts
@@ -12,33 +12,34 @@ export type SquiggleLangPackageTypes = {
   "0.9.2": GetImportType<typeof import("squiggle-lang-0.9.2")>;
   dev: GetImportType<typeof import("@quri/squiggle-lang")>;
 };
-
 export async function squiggleLangByVersion<T extends SquiggleVersion>(
   version: T
 ): Promise<SquiggleLangPackageTypes[T]> {
   // Auto-generated, don't touch.
   // Enumerating all imports is necessary; `await import(version)` won't be enough.
+  // Note: `as unknown` casting here is not strictly necessary, but it matches the code in `versionedSquiggleComponents`,
+  // which helps with `patch-js.ts` transformations.
   switch (version) {
     case "0.8.5":
       return (await import(
         "squiggle-lang-0.8.5"
-      )) as SquiggleLangPackageTypes[T];
+      )) as unknown as SquiggleLangPackageTypes[T];
     case "0.8.6":
       return (await import(
         "squiggle-lang-0.8.6"
-      )) as SquiggleLangPackageTypes[T];
+      )) as unknown as SquiggleLangPackageTypes[T];
     case "0.9.0":
       return (await import(
         "squiggle-lang-0.9.0"
-      )) as SquiggleLangPackageTypes[T];
+      )) as unknown as SquiggleLangPackageTypes[T];
     case "0.9.2":
       return (await import(
         "squiggle-lang-0.9.2"
-      )) as SquiggleLangPackageTypes[T];
+      )) as unknown as SquiggleLangPackageTypes[T];
     case "dev":
       return (await import(
         "@quri/squiggle-lang"
-      )) as SquiggleLangPackageTypes[T];
+      )) as unknown as SquiggleLangPackageTypes[T];
     default:
       throw new Error(`Unkonwn version ${version satisfies never}`);
   }
