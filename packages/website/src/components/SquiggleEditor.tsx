b725491bd78396af44675f25e2db1b406bea69b2
Ozzie Gooen <ozzieagooen@gmail.com>
2024-01-27 09:19:00 -0800

    Fix for website

--- packages/website/src/components/SquiggleEditor.tsx
+++ packages/website/src/components/SquiggleEditor.tsx
@@ -1,10 +1,11 @@
-import { SquiggleEditor as OriginalSquiggleEditor } from "@quri/squiggle-components";
 import { FC } from "react";
+
+import { SquiggleEditor as OriginalSquiggleEditor } from "@quri/squiggle-components";
 
 export const SquiggleEditor: FC<
   Parameters<typeof OriginalSquiggleEditor>[0]
 > = (props) => (
   <div className="mt-6">
-    <OriginalSquiggleEditor {...props} chartHeight={30} />
+    <OriginalSquiggleEditor {...props} settings={{ chartHeight: 30 }} />
   </div>
 );
