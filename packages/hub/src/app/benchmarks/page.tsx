c4ca99893760679cc169815976a66eae53b4509e
Vyacheslav Matyukhin <me@berekuk.ru>
2024-02-05 19:53:04 -0600

    benchmark page improvements

--- packages/hub/src/app/benchmarks/page.tsx
+++ packages/hub/src/app/benchmarks/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { fromByteArray } from "base64-js";
 import { deflate } from "pako";
-import { FC, Fragment, useEffect, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-import { CheckIcon, ErrorIcon, RefreshIcon } from "@quri/ui";
+import { Button, CheckIcon, ErrorIcon, RefreshIcon } from "@quri/ui";
 import {
   SquiggleVersion,
   squiggleVersions,
@@ -11,7 +13,7 @@ import {
 } from "@quri/versioned-squiggle-components";
 
 import { NarrowPageLayout } from "@/components/layout/NarrowPageLayout";
-import { H2 } from "@/components/ui/Headers";
+import { H1, H2 } from "@/components/ui/Headers";
 
 function getPlaygroundUrl(version: SquiggleVersion, code: string) {
   const HASH_PREFIX = "#code=";
@@ -24,83 +26,161 @@ function getPlaygroundUrl(version: SquiggleVersion, code: string) {
   );
 }
 
-const Benchmark: FC<{ code: string }> = ({ code }) => {
-  type Outcome = {
-    time: number;
-    ok: boolean;
-  };
+type Outcome = {
+  time: number;
+  ok: boolean;
+};
+type Task = {
+  code: string;
+  version: SquiggleVersion;
+  onStart: () => void;
+  onFinish: (outcome: Outcome) => void;
+};
 
-  const [outcomes, setOutcomes] = useState<
-    Partial<Record<SquiggleVersion, Outcome>>
-  >({});
+class Queue {
+  tasks: Task[] = [];
+  processing: boolean = false;
+  processed: number = 0;
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+    this.process(); // lack of await is intentional
+  }
+
+  async process() {
+    if (this.processing) {
+      return;
+    }
+    const task = this.tasks[this.processed];
+    if (!task) {
+      return;
+    }
+    this.processing = true;
+    task.onStart();
+
+    const squiggle = await versionedSquigglePackages(task.version);
+    const project = squiggle.lang.SqProject.create();
+    project.setSource("main", task.code);
+
+    const started = new Date();
+    await project.run("main");
+    const result = project.getResult("main");
+    const ended = new Date();
+
+    const ms = ended.getTime() - started.getTime();
+
+    task.onFinish({
+      ok: result.ok,
+      time: ms / 1000,
+    });
+
+    this.processed++;
+    this.processing = false;
+
+    // try; it won't do anything if queue is empty
+    setTimeout(() => this.process(), 5);
+  }
+}
+
+// singleton; could be wrapped in a Provider
+const queue = new Queue();
+
+const Benchmark: FC<{ version: SquiggleVersion; code: string }> = ({
+  version,
+  code,
+}) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [outcome, setOutcome] = useState<Outcome | undefined>();
+
+  const run = useCallback(() => {
+    queue.addTask({
+      version,
+      code,
+      onStart: () => {
+        setIsProcessing(true);
+      },
+      onFinish: (outcome) => {
+        setIsProcessing(false);
+        setOutcome(outcome);
+      },
+    });
+  }, [code, version]);
 
   useEffect(() => {
-    const run = async () => {
-      for (const version of squiggleVersions) {
-        const squiggle = await versionedSquigglePackages(version);
-        const project = squiggle.lang.SqProject.create();
-        project.setSource("main", code);
-
-        const started = new Date();
-        await project.run("main");
-        const result = project.getResult("main");
-        const ended = new Date();
-
-        const ms = ended.getTime() - started.getTime();
-        setOutcomes((outcomes) => ({
-          ...outcomes,
-          [version]: {
-            time: ms / 1000,
-            ok: result.ok,
-          },
-        }));
-      }
-    };
-
     run();
-  }, [code]);
+  }, [run]);
 
+  return (
+    <div className="grid grid-cols-subgrid col-span-2">
+      <div className="text-slate-700 font-medium">{version}</div>
+      {isProcessing ? (
+        <RefreshIcon className="text-slate-500 animate-spin" />
+      ) : outcome ? (
+        <div className="flex gap-1 items-center">
+          {outcome.ok ? (
+            <CheckIcon className="text-green-700" />
+          ) : (
+            <ErrorIcon className="text-red-500" />
+          )}
+          <a
+            href={getPlaygroundUrl(version, code)}
+            className="text-blue-500 hover:underline"
+          >
+            {outcome.time}s
+          </a>
+          <Button size="small" onClick={run}>
+            Run again
+          </Button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+const BenchmarkTable: FC<{ code: string }> = ({ code }) => {
   return (
     <div>
       <H2>{code}</H2>
-      <div className="inline-grid grid-cols-2 gap-2">
+      <div className="inline-grid grid-cols-[auto_auto] gap-x-6 gap-y-2">
         {squiggleVersions.map((version) => (
-          <Fragment key={version}>
-            <div className="text-slate-700 font-medium">{version}</div>
-            {outcomes[version] ? (
-              <div className="flex gap-1 items-center">
-                {outcomes[version]!.ok ? (
-                  <CheckIcon className="text-green-700" />
-                ) : (
-                  <ErrorIcon className="text-red-500" />
-                )}
-                <a
-                  href={getPlaygroundUrl(version, code)}
-                  className="text-blue-500 hover:underline"
-                >
-                  {outcomes[version]!.time}s
-                </a>
-              </div>
-            ) : (
-              <RefreshIcon className="text-slate-500 animate-spin" />
-            )}
-          </Fragment>
+          <Benchmark key={version} code={code} version={version} />
         ))}
       </div>
     </div>
   );
 };
 
+/**
+ * TODO:
+ * - run on server
+ * - save results in the database
+ * - run multiple times and average the results
+ * - more benchmarks
+ */
 export default function BenchmarksPage() {
   const codes = [
-    "List.upTo(1, 50000) -> map({|i| i to i + 1}) -> List.length",
-    "X = 5",
+    "List.upTo(1, 10000) -> map({ |i| i to i + 1 }) -> List.length",
+    "List.upTo(1, 1e7) -> map({ |i| i })",
+    "List.upTo(1, 1e4) -> map({|x| List.upTo(1, 100) -> reduce(0, {|a,b|a+b})})",
   ];
   return (
     <NarrowPageLayout>
+      <H1>Squiggle benchmarks</H1>
       <div className="space-y-8">
+        <ReactMarkdown className="prose max-w-4xl" remarkPlugins={[remarkGfm]}>
+          {`
+Important considerations:
+
+- Keep this page in the foreground when you run benchmarks, otherwise your browser or OS might throttle the execution
+- JS engines JIT hot functions after a second or so, so re-runs will be faster than the initial run; this page doesn't account for that yet and doesn't try to warm up the JS engine
+    - JIT will happen for each Squiggle version separately
+- Some variance between runs is expected, try not to over-update on small differences
+- These benchmarks are memory-intense; also, JS will run garbage collection after the test has finished
+`}
+        </ReactMarkdown>
+        <hr />
         {codes.map((code) => (
-          <Benchmark key={code} code={code} />
+          <BenchmarkTable key={code} code={code} />
         ))}
       </div>
     </NarrowPageLayout>
