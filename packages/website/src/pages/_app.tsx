c89f6579f3dcd99f2f055f0700c1e9ff5174b9a8
Vyacheslav Matyukhin <me@berekuk.ru>
2024-02-14 10:39:48 -0600

    enable vercel analytics

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@quri/squiggle-components/common.css";
 import "../styles/main.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
