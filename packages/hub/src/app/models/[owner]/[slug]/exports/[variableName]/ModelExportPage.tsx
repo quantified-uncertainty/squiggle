89f03ac77e41e8891974d0766654a96e90ad01a4
Ozzie Gooen <ozzieagooen@gmail.com>
2024-04-01 21:11:24 -0700

    Model export version changer

--- packages/hub/src/app/models/[owner]/[slug]/exports/[variableName]/ModelExportPage.tsx
+++ packages/hub/src/app/models/[owner]/[slug]/exports/[variableName]/ModelExportPage.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { FC } from "react";
+import clsx from "clsx";
+import { format } from "date-fns";
+import { FC, useState } from "react";
 import { graphql } from "react-relay";
 
 import { extractFromGraphqlErrorUnion } from "@/lib/graphqlHelpers";
@@ -56,18 +58,56 @@ export const ModelExportPage: FC<{
   );
 
   const model = extractFromGraphqlErrorUnion(result, "Model");
-  const content = model.currentRevision.content;
 
-  switch (content.__typename) {
-    case "SquiggleSnippet": {
-      return (
-        <SquiggleModelExportPage
-          variableName={params.variableName}
-          contentRef={content}
-        />
-      );
+  const [selected, changeId] = useState<string>(model.exportRevisions[0].id);
+
+  const content = model.exportRevisions.find(
+    (revision) => revision.id === selected
+  )?.modelRevision.content;
+
+  if (content) {
+    switch (content.__typename) {
+      case "SquiggleSnippet": {
+        return (
+          <div className="flex">
+            <div className="flex-1 w-full">
+              <SquiggleModelExportPage
+                variableName={params.variableName}
+                contentRef={content}
+                key={selected}
+              />
+            </div>
+            <div className="w-[200px] ml-4">
+              <div className="flex flex-col">
+                <h3 className="text-sm font-medium text-gray-700 border-b mb-1 pb-0.5">
+                  Revisions
+                </h3>
+                <ul>
+                  {model.exportRevisions.map((revision) => (
+                    <li
+                      key={revision.id}
+                      onClick={() => changeId(revision.id)}
+                      className={clsx(
+                        "hover:text-gray-900 cursor-pointer hover:underline text-sm pt-0.5 pb-0.5",
+                        revision.id === selected
+                          ? "text-blue-900"
+                          : "text-gray-400"
+                      )}
+                    >
+                      {format(
+                        new Date(revision.modelRevision.createdAtTimestamp),
+                        "MMM dd, yyyy"
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        );
+      }
+      default:
+        return <div>Unknown model type {content.__typename}</div>;
     }
-    default:
-      return <div>Unknown model type {content.__typename}</div>;
   }
 };
