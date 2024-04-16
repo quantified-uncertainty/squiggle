c406d81789e0b3b1775ca98a7ca08192133dfc9a
Ozzie Gooen <ozzieagooen@gmail.com>
2024-04-15 21:25:13 -0700

    More card cleanup

--- packages/hub/src/variables/components/VariableCard.tsx
+++ packages/hub/src/variables/components/VariableCard.tsx
@@ -1,4 +1,3 @@
-import truncate from "lodash/truncate";
 import Link from "next/link";
 import { FC } from "react";
 import { useFragment } from "react-relay";
@@ -7,7 +6,13 @@ import { graphql } from "relay-runtime";
 import { MarkdownViewer } from "@quri/squiggle-components";
 import { CodeBracketSquareIcon } from "@quri/ui";
 
-import { PrivateBadge, UpdatedStatus } from "@/components/EntityCard";
+import {
+  Badge,
+  badgeCss,
+  keepFirstNLines,
+  PrivateBadge,
+  UpdatedStatus,
+} from "@/components/EntityCard";
 import { exportTypeIcon } from "@/lib/typeIcon";
 import { modelRoute, variableRoute } from "@/routes";
 
@@ -52,23 +57,15 @@ export const VariableCard: FC<Props> = ({ variableRef }) => {
   const Icon = exportTypeIcon(currentRevision.variableType || "");
 
   // This will have problems with markdown tags, but I looked into markdown-truncation packages, and they can get complicated. Will try this for now.
-  const docstring =
-    (currentRevision.docstring &&
-      truncate(currentRevision.docstring, {
-        length: 500,
-        separator: " ",
-        omission: "...",
-      })) ||
-    undefined;
 
   const { createdAtTimestamp } = currentRevision.modelRevision;
 
   return (
-    <>
-      <div className="flex flex-col overflow-hidden">
-        <div className="mb-1 px-4">
+    <div className="flex flex-col">
+      <div className="flex-grow px-2">
+        <div className="mb-1">
           <Link
-            className="text-gray-900 font-medium hover:underline"
+            className="font-medium text-gray-900 hover:underline"
             href={variableRoute({
               modelSlug: variable.model.slug,
               variableName: variable.variableName,
@@ -78,9 +75,9 @@ export const VariableCard: FC<Props> = ({ variableRef }) => {
             {variable.currentRevision?.title || variable.variableName}
           </Link>
         </div>
-        <div className="flex flex-wrap items-center gap-x-4 gap-y-2 text-gray-500 text-xs mb-3 px-4 overflow-hidden">
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-1 text-xs text-gray-500">
           <a
-            className="cursor-pointer items-center flex text-xs text-gray-500 hover:text-gray-900 hover:underline"
+            className={badgeCss(true)}
             href={modelRoute({
               owner: variable.owner.slug,
               slug: variable.model.slug,
@@ -89,25 +86,25 @@ export const VariableCard: FC<Props> = ({ variableRef }) => {
             <CodeBracketSquareIcon size={12} className="mr-1" />
             {`${variable.owner.slug}/${variable.model.slug}`}
           </a>
-          <div className="items-center flex text-xs text-gray-500">
+          <Badge presentAsLink={false}>
             <Icon size={10} className="mr-1" />
             {currentRevision.variableType}
-          </div>
+          </Badge>
           <UpdatedStatus time={createdAtTimestamp} />
           {variable.model.isPrivate && <PrivateBadge />}
         </div>
-        {docstring && (
-          <div className="border border-gray-200 rounded-md">
-            <div className="px-4 py-1 overflow-hidden text-xs">
-              <MarkdownViewer
-                md={docstring}
-                textSize="xs"
-                background-color=""
-              />
-            </div>
-          </div>
-        )}
       </div>
-    </>
+      {currentRevision.docstring && (
+        <div className="mt-3 rounded-md bg-gray-50">
+          <div className="px-2 py-1 text-xs text-gray-600">
+            <MarkdownViewer
+              md={keepFirstNLines(currentRevision.docstring, 10)}
+              textSize="xs"
+              background-color=""
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
