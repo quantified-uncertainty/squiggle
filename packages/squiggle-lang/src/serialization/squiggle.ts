cdcf9553355f62c382ec0239a81845a2d5e290f4
Vyacheslav Matyukhin <me@berekuk.ru>
2024-04-18 16:13:43 -0300

    squiggleCodec usage comment

--- packages/squiggle-lang/src/serialization/squiggle.ts
+++ packages/squiggle-lang/src/serialization/squiggle.ts
@@ -74,6 +74,31 @@ export type SquiggleDeserializationVisitor =
 export type SquiggleBundleEntrypoint<T extends keyof SquiggleShape> =
   BundleEntrypoint<SquiggleShape, T>;
 
+/**
+ * This is the main entrypoint for the entire serialization subsystem.
+ *
+ * How to use:
+ * ```
+ * const serializer = squiggleCodec.makeSerializer(); // make a serializer that contains a single bundle and can serialize multiple values
+ *
+ * // you can throw multiple things in the bundle, just don't forget to track the entrypoints
+ * const entrypoint1 = serializer.serialize("value", myValue); // serialize a value
+ * const entrypoint2 = serializer.serialize("value", myValue2); // serialize another value
+ * const entrypoint3 = serializer.serialize("expression", expression); // serialize an expression (or any other entity type that's supported)
+ *
+ * // get the bundle - it will contain everything that was serialized
+ * const bundle = serializer.getBundle();
+ *
+ * // Now, both `bundle` and `entrypoint1`, `entrypoint2`, `entrypoint3` can be sent over the wire or saved to disk or database. They're just JSON objects.
+ *
+ * // On the other side, after you restore them:
+ * const deserializer = squiggleCodec.makeDeserializer(bundle);
+ * const myValue = deserializer.deserialize(entrypoint1);
+ * // ... deserialize other values based on their entrypoints
+ * ```
+ *
+ * Real-world usage examples can be found in `__tests__/serialization_test.ts` and in `EmbeddedWithSerializationRunner.ts`.
+ */
 export const squiggleCodec = {
   makeSerializer: () => new SquiggleSerializationStore(),
   makeDeserializer: (bundle: SquiggleBundle) =>
