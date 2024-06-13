d6439dc6942d2962f315f2de2be397939a42ae32
Vyacheslav Matyukhin <me@berekuk.ru>
2024-06-13 15:51:12 -0300

    refactor worker runners

---
+++ packages/squiggle-lang/src/runners/AnyWorkerRunner.ts
@@ -0,0 +1,50 @@
+import { squiggleCodec } from "../serialization/squiggle.js";
+import { BaseRunner, RunParams, RunResult } from "./BaseRunner.js";
+import { deserializeRunResult } from "./serialization.js";
+import { SquiggleWorkerJob, SquiggleWorkerResponse } from "./worker.js";
+
+export async function runWithWorker(
+  { environment, ast, externals }: RunParams,
+  worker: Worker
+): Promise<RunResult> {
+  const store = squiggleCodec.makeSerializer();
+  const externalsEntrypoint = store.serialize("value", externals);
+  const bundle = store.getBundle();
+
+  worker.postMessage({
+    environment,
+    ast,
+    bundle,
+    externalsEntrypoint,
+  } satisfies SquiggleWorkerJob);
+
+  return new Promise<RunResult>((resolve) => {
+    worker.addEventListener(
+      "message",
+      (e: MessageEvent<SquiggleWorkerResponse>) => {
+        if (e.data.type === "internal-error") {
+          throw new Error(`Internal worker error: ${e.data.payload}`);
+        }
+        if (e.data.type !== "result") {
+          throw new Error(
+            `Unexpected message ${JSON.stringify(e.data)} from worker`
+          );
+        }
+        const { payload } = e.data;
+
+        worker.terminate();
+        resolve(deserializeRunResult(payload));
+      }
+    );
+  });
+}
+
+export abstract class AnyWorkerRunner extends BaseRunner {
+  async run(params: RunParams): Promise<RunResult> {
+    const worker = await this.getWorker();
+
+    return await runWithWorker(params, worker);
+  }
+
+  abstract getWorker(): Promise<Worker>;
+}
--- packages/squiggle-lang/src/runners/WebWorkerRunner.ts
+++ packages/squiggle-lang/src/runners/WebWorkerRunner.ts
@@ -1,45 +1,6 @@
-import { squiggleCodec } from "../serialization/squiggle.js";
-import { BaseRunner, RunParams, RunResult } from "./BaseRunner.js";
-import { deserializeRunResult } from "./serialization.js";
-import { SquiggleWorkerJob, SquiggleWorkerResponse } from "./worker.js";
+import { AnyWorkerRunner } from "./AnyWorkerRunner.js";
 
-export async function runWithWorker(
-  { environment, ast, externals }: RunParams,
-  worker: Worker
-): Promise<RunResult> {
-  const store = squiggleCodec.makeSerializer();
-  const externalsEntrypoint = store.serialize("value", externals);
-  const bundle = store.getBundle();
-
-  worker.postMessage({
-    environment,
-    ast,
-    bundle,
-    externalsEntrypoint,
-  } satisfies SquiggleWorkerJob);
-
-  return new Promise<RunResult>((resolve) => {
-    worker.addEventListener(
-      "message",
-      (e: MessageEvent<SquiggleWorkerResponse>) => {
-        if (e.data.type === "internal-error") {
-          throw new Error(`Internal worker error: ${e.data.payload}`);
-        }
-        if (e.data.type !== "result") {
-          throw new Error(
-            `Unexpected message ${JSON.stringify(e.data)} from worker`
-          );
-        }
-        const { payload } = e.data;
-
-        worker.terminate();
-        resolve(deserializeRunResult(payload));
-      }
-    );
-  });
-}
-
-export class WebWorkerRunner extends BaseRunner {
+export class WebWorkerRunner extends AnyWorkerRunner {
   constructor() {
     // This runner is browser-specific.
     if (typeof window === "undefined") {
@@ -48,11 +9,9 @@ export class WebWorkerRunner extends BaseRunner {
     super();
   }
 
-  async run(params: RunParams): Promise<RunResult> {
-    const worker = new Worker(new URL("./worker.js", import.meta.url), {
+  async getWorker(): Promise<Worker> {
+    return new Worker(new URL("./worker.js", import.meta.url), {
       type: "module",
     });
-
-    return await runWithWorker(params, worker);
   }
 }
