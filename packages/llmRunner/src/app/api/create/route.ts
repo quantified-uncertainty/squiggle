347d5e9bb52e6d44ecfb5cb074717e0af7fef563
Your Name <you@example.com>
2024-09-06 11:14:58 -0700

    Changed maxDuration, to fix llmRunner build

--- packages/llmRunner/src/app/api/create/route.ts
+++ packages/llmRunner/src/app/api/create/route.ts
@@ -5,7 +5,7 @@ import {
   SquiggleResponse,
 } from "../../utils/squiggleTypes";
 
-export const maxDuration = 500;
+export const maxDuration = 300;
 
 export async function POST(req: Request) {
   const abortController = new AbortController();
