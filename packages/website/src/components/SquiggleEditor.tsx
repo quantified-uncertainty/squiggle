1f099565b5652a85f4a2d8f2d004c6c8f7985655
Vyacheslav Matyukhin <me@berekuk.ru>
2024-10-24 15:24:04 -0300

    comment about next.js error

--- packages/website/src/components/SquiggleEditor.tsx
+++ packages/website/src/components/SquiggleEditor.tsx
@@ -3,6 +3,12 @@ import { FC } from "react";
 
 import { SquiggleEditor as OriginalSquiggleEditor } from "@quri/squiggle-components";
 
+/* FIXME: using this component with Fumadocs/Next.js/MDX is causing an internal error:
+ *
+ * > Internal error: Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.
+ *
+ * But the component renders correctly in the end, so I'm not sure what's going on.
+ */
 export const SquiggleEditor: FC<
   Parameters<typeof OriginalSquiggleEditor>[0]
 > = (props) => (
