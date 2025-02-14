064d91af59e24bfaa177af86efc9f67b4db3af6a
Vyacheslav Matyukhin <me@berekuk.ru>
2025-02-14 16:21:13 -0300

    higher tracing root

--- apps/metaforecast/next.config.mjs
+++ apps/metaforecast/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 export default {
   // this includes files from the monorepo base two directories up
-  // outputFileTracingRoot: path.join(import.meta.dirname, "../../"),
+  outputFileTracingRoot: path.join(import.meta.dirname, "../../"),
   outputFileTracingIncludes: {
     "/*": ["./node_modules/@quri/metaforecast-db/generated/*.node"],
   },
