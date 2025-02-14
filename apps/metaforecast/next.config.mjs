b2a85efb415447ea9c404480073d400dda628179
Vyacheslav Matyukhin <me@berekuk.ru>
2025-02-14 17:19:13 -0300

    without tracing configs

--- apps/metaforecast/next.config.mjs
+++ apps/metaforecast/next.config.mjs
@@ -1,13 +1,12 @@
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-import path from "path";
 
 /** @type {import('next').NextConfig} */
 export default {
   // this includes files from the monorepo base two directories up
-  outputFileTracingRoot: path.join(import.meta.dirname, "../../"),
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/@quri/metaforecast-db/generated/*.node"],
-  },
+  // outputFileTracingRoot: path.join(import.meta.dirname, "../../"),
+  // outputFileTracingIncludes: {
+  //   "/*": ["./node_modules/@quri/metaforecast-db/generated/*.node"],
+  // },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
