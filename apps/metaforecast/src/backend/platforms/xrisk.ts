72046e296e157de864416d83a3b1e96e9a109345
Vyacheslav Matyukhin <me@berekuk.ru>
2025-02-24 16:03:22 -0300

    cleanup xrisk platform scripts; remove legacy algolia config

--- apps/metaforecast/src/backend/platforms/xrisk.ts
+++ apps/metaforecast/src/backend/platforms/xrisk.ts
@@ -5,6 +5,10 @@ import { hash } from "../utils/hash";
 
 const platformName = "xrisk";
 
+/*
+ * Original source of data: https://docs.google.com/spreadsheets/d/1W10B6NJjicD8O0STPiT3tNV3oFnT8YsfjmtYR8RO_RI/edit?gid=0#gid=0
+ */
+
 export const xrisk: Platform = {
   name: "xrisk",
   label: "X-risk estimates",
@@ -12,10 +16,10 @@ export const xrisk: Platform = {
 
   async fetcher() {
     // return; // not necessary to refill the DB every time
-    let fileRaw = fs.readFileSync("./input/xrisk-questions.json", {
+    const fileRaw = fs.readFileSync("./input/xrisk-questions.json", {
       encoding: "utf-8",
     });
-    let parsedData = JSON.parse(fileRaw);
+    const parsedData = JSON.parse(fileRaw);
     const results = parsedData.map((item: any) => {
       item.extra = item.moreoriginsdata;
       delete item.moreoriginsdata;
