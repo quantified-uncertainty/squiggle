f4389face3e4d3817009c87e983536e61dbefbd4
Vyacheslav Matyukhin <me@berekuk.ru>
2025-02-27 23:19:57 -0300

    css fix

--- apps/fermi-contest-2025-02/tailwind.config.js
+++ apps/fermi-contest-2025-02/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
