6d28b9127cb67205fc782e0bb70e3672adf845ac
Vyacheslav Matyukhin <me@berekuk.ru>
2025-03-12 00:29:37 -0300

    remove unused code

--- apps/hub/src/evals/data/evals.tsx
+++ apps/hub/src/evals/data/evals.tsx
@@ -1,9 +1,7 @@
-import { Eval, EvalResult, Prisma, Spec } from "@quri/hub-db";
+import { EvalResult, Prisma, Spec } from "@quri/hub-db";
 
 import { prisma } from "@/lib/server/prisma";
 import { checkRootUser } from "@/users/auth";
-
-import { SpecList } from "./specLists";
 
 // Selection for fetching eval data with detailed information
 export const evalSelectWithDetails = {
@@ -15,7 +13,7 @@ export const evalSelectWithDetails = {
       id: true,
       name: true,
       type: true,
-    }
+    },
   },
   specList: {
     select: {
@@ -59,7 +57,7 @@ export async function getAllEvals() {
         select: {
           id: true,
           name: true,
-        }
+        },
       },
       specList: {
         select: {
@@ -93,7 +91,7 @@ export async function getEvalsBySpecListId(specListId: string) {
         select: {
           id: true,
           name: true,
-        }
+        },
       },
       _count: {
         select: {
@@ -121,60 +119,3 @@ type EvaluatorResult = Pick<EvalResult, "specId" | "code" | "workflowId">;
 // Evaluators are pluggable.
 // The main one is Squiggle AI evaluator, but we can add more (e.g. prompting a human to evaluate, or loading resolved results from Metaforecast database).
 export type Evaluator = (spec: Spec) => Promise<EvaluatorResult>;
-
-export async function runEvalOnSpecList(
-  specList: SpecList,
-  evaluator: Evaluator
-): Promise<Eval> {
-  await checkRootUser();
-
-  // create a new Eval record
-  const evaluation = await prisma.eval.create({
-    data: {
-      evaluator: "anonymous", // TODO: named evaluators
-      specList: {
-        connect: {
-          id: specList.id,
-        },
-      },
-    },
-  });
-
-  // TODO - bring back Promise.all
-  for (const spec of specList.specs) {
-    const result = await evaluator(spec.spec);
-
-    // store result in db
-    await prisma.evalResult.create({
-      data: {
-        spec: {
-          connect: {
-            id: spec.spec.id,
-          },
-        },
-        code: result.code,
-        eval: {
-          connect: {
-            id: evaluation.id,
-          },
-        },
-        ...(result.workflowId
-          ? {
-              workflow: {
-                connect: {
-                  id: result.workflowId,
-                },
-              },
-            }
-          : {}),
-      },
-    });
-  }
-
-  // reselect the Eval record from db
-  return await prisma.eval.findUniqueOrThrow({
-    where: {
-      id: evaluation.id,
-    },
-  });
-}
