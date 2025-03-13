007e1b60df4fcc801b8c2660392f0565e3b64581
Vyacheslav Matyukhin <me@berekuk.ru>
2025-03-13 14:04:07 -0300

    playground url utils in versioned-components

--- apps/website/src/app/playground/ClientPlayground.tsx
+++ apps/website/src/app/playground/ClientPlayground.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { fromByteArray, toByteArray } from "base64-js";
 import { useSearchParams } from "next/navigation";
-import { deflate, inflate } from "pako";
-import { FC, use, useState } from "react";
+import { FC, use, useEffect, useState } from "react";
 
 import {
+  checkSquiggleVersion,
   defaultSquiggleVersion,
+  getPlaygroundUrl,
+  parsePlaygroundUrl,
+  type PlaygroundParams,
   SquigglePlaygroundVersionPicker,
   type SquiggleVersion,
   squiggleVersions,
@@ -16,53 +18,38 @@ import {
 import { useAvailableHeight } from "../../utils/useAvailableHeight";
 import { ShareButton } from "./ShareButton";
 
-const HASH_PREFIX = "#code=";
-function getHashData() {
-  if (typeof window === "undefined") {
-    return {};
-  }
-  const hash = window.location.hash;
-  if (!hash.startsWith(HASH_PREFIX)) {
-    return {};
-  }
-  try {
-    const compressed = toByteArray(
-      decodeURIComponent(hash.slice(HASH_PREFIX.length))
-    );
-    const text = inflate(compressed, { to: "string" });
-    return JSON.parse(text);
-  } catch (err) {
-    console.error(err);
-    return {};
-  }
-}
+function updateUrl(params: Partial<PlaygroundParams>) {
+  const url = new URL(window.location.href);
+  const newUrl = getPlaygroundUrl({ ...parsePlaygroundUrl(url), ...params });
 
-type Data = {
-  initialSquiggleCode?: string; // legacy
-  defaultCode?: string;
-  showSummary?: boolean;
-};
+  // Easier than removing the origin prefix, but is it reliable?
+  newUrl.host = url.host;
+  newUrl.protocol = url.protocol;
 
-function updateUrl(data: Partial<Data>, version: SquiggleVersion) {
-  const text = JSON.stringify({ ...getHashData(), ...data });
-  const compressed = deflate(text, { level: 9 });
-  window.history.replaceState(
-    undefined,
-    "",
-    `/playground?v=${version}` +
-      HASH_PREFIX +
-      encodeURIComponent(fromByteArray(compressed))
-  );
+  // Intentionally not triggering Next.js navigation / re-render.
+  window.history.replaceState(undefined, "", newUrl.toString());
 }
 
 export const ClientPlayground: FC = () => {
   const params = useSearchParams();
 
-  const hashData = getHashData();
-  if (hashData.initialSquiggleString) {
-    hashData.defaultCode = String(hashData.initialSquiggleString);
-    delete hashData.initialSquiggleString;
-  }
+  useEffect(() => {
+    // normalize the URL once
+    updateUrl({});
+  }, []);
+
+  const [initialParams] = useState<PlaygroundParams>(() => {
+    if (typeof window === "undefined") {
+      const v = params.get("v");
+      if (v && checkSquiggleVersion(v)) {
+        return { version: v };
+      } else {
+        return {};
+      }
+    }
+    const url = new URL(window.location.href);
+    return parsePlaygroundUrl(url);
+  });
 
   // infinite spinner
   const bannedVersions = ["0.9.4", "0.9.5"];
@@ -73,20 +60,23 @@ export const ClientPlayground: FC = () => {
   const [version, setVersion] = useState<SquiggleVersion>(() => {
     // TODO - replace with `useAdjustedSquiggleVersion`
     for (const version of squiggleVersions) {
-      if (params.get("v") === version && !bannedVersions.includes(version)) {
+      if (
+        initialParams.version === version &&
+        !bannedVersions.includes(version)
+      ) {
         return version;
       }
     }
-    if (params.get("v") && typeof window !== "undefined") {
+    if (initialParams.version && typeof window !== "undefined") {
       // wrong version, let's replace it
-      updateUrl({}, defaultUnbannedVersion);
+      updateUrl({ version: defaultUnbannedVersion });
     }
     return defaultUnbannedVersion;
   });
 
   const onVersionChange = (version: SquiggleVersion) => {
     setVersion(version);
-    updateUrl({}, version);
+    updateUrl({ version });
   };
 
   const { height, ref } = useAvailableHeight();
@@ -97,9 +87,9 @@ export const ClientPlayground: FC = () => {
     <div className="bg-white" ref={ref}>
       <squiggle.components.SquigglePlayground
         height={height}
-        defaultCode={hashData.defaultCode ?? "a = normal(0, 1)"}
+        defaultCode={initialParams.code ?? "a = normal(0, 1)"}
         distributionChartSettings={{
-          showSummary: hashData.showSummary ?? true,
+          showSummary: initialParams.showSummary ?? true,
         }}
         renderExtraControls={() => (
           <div className="flex h-full items-center justify-end gap-2">
@@ -111,10 +101,10 @@ export const ClientPlayground: FC = () => {
             />
           </div>
         )}
-        onCodeChange={(code) => updateUrl({ defaultCode: code }, version)}
+        onCodeChange={(code) => updateUrl({ code })}
         onSettingsChange={(settings) => {
           const showSummary = settings.distributionChartSettings?.showSummary;
-          updateUrl({ showSummary }, version);
+          updateUrl({ showSummary });
         }}
       />
     </div>
