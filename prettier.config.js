3987cdf9355456239f1e281d2c1b126e6576b416
Michael Ravits <michaelr524@gmail.com>
2025-11-02 21:14:14 +0000

    .

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
   trailingComma: "es5",
-  semi: true,
-  endOfLine: "lf",
-  printWidth: 160,
-  singleAttributePerLine: false,
-  objectWrap: "collapse",
   plugins: ["prettier-plugin-tailwindcss"],
   overrides: [
     {
